bd2a14c9a6ae6066776d34e04b9d51250e296b17
ForsanNemah <forsan20172017@gmail.com>
2022-12-30 13:34:25 +0300

    Update info.js

--- hokeel/moh/info.js
+++ hokeel/moh/info.js
@@ -5,7 +5,7 @@
 var phn="966598123444";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
-var action_url="https://script.google.com/macros/s/AKfycbwn6ZgLVkZhq92ZfJghZxp478s3W6FaL-EhcinuT9wXbsB9ieEL8w2ZGxQiPXohBFudcA/exec";
+var action_url="https://script.google.com/macros/s/AKfycbyVoh6GUpnJFzSmePZOzd_I6QpREYfNQWUE28fqSE4brHMIzJGkWHQ9d7N-25L0uN4DTg/exec";
 
  
 //alert(phn);
