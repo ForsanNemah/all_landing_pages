846fcc8622340b6bac50dca16964b399a0881e14
ForsanNemah <forsan20172017@gmail.com>
2023-01-03 12:23:40 +0300

    w sendr landing

---
+++ w_sender_landing_page/info.js
@@ -0,0 +1,19 @@
+ 
+
+
+
+var phn="966553907575";
+var msg="تم التسجيل بنجاح ";
+var user_id="2";
+var action_url="https://script.google.com/macros/s/AKfycby8-WAj28aQNyyLxrEyfZ7IlsYVwhFjSlVLzv1drbgqOdmrxmdlkcaaSd2Hh0UxNDTCLw/exec";
+
+ 
+//alert(phn);
+
+
+
+
+
+
+
+ 
