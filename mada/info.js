ec5a6b8cd5c099106736596e31dfc26b0dcf9d2c
ForsanNemah <forsan20172017@gmail.com>
2023-01-05 16:20:19 +0300

    add mada

---
+++ mada/info.js
@@ -0,0 +1,19 @@
+ 
+
+
+
+var phn="966566634800";
+var msg="تم التسجيل بنجاح ";
+var user_id="2";
+var action_url="https://script.google.com/macros/s/AKfycbxZWklJd6QrnlFQlim7BAIj0Wj7zbiK1TyWv43I3mWhKxO51nnmvka0rjQcfqHzdoAL/exec";
+
+ 
+//alert(phn);
+
+
+
+
+
+
+
+ 
