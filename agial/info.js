318788bca92504b3eabc02ffcd9a5c34b3cf3735
ForsanNemah <forsan20172017@gmail.com>
2023-01-05 14:29:17 +0300

    agial

---
+++ agial/info.js
@@ -0,0 +1,19 @@
+ 
+
+
+
+var phn="966557211726";
+var msg="تم التسجيل بنجاح ";
+var user_id="2";
+var action_url="https://script.google.com/macros/s/AKfycbyfyXhaUTp2WQvBbeJMlU1SLBAfulowd25H7GxFQ6Bss0VH25ej8XnFuILScxXc5GvfWA/exec";
+
+ 
+//alert(phn);
+
+
+
+
+
+
+
+ 
