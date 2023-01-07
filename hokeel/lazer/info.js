4b6f03a2233573491479e57d211422d1a9a9d1a6
ForsanNemah <forsan20172017@gmail.com>
2023-01-07 16:06:26 +0300

    lazer url

--- hokeel/lazer/info.js
+++ hokeel/lazer/info.js
@@ -5,7 +5,7 @@
 var phn="966598123444";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
-var action_url="https://script.google.com/macros/s/AKfycby3ky8cIfhybvxoM8btai6kQXitC22-ysxN0eFZaZY14_ncnA2C2WLm0JNSZN2bjsYx/exec";
+var action_url="https://script.google.com/macros/s/AKfycbxtA8fXW2AKOwL05alTT9iozFoC5jxk80nyGke_fmbxfxdFAm3mkxs4jqG011TAlnL5/exec";
 
  
 //alert(phn);
