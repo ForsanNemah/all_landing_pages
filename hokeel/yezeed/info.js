b88d8f55e004fe06a85db07e4f0e65a1c8cc9505
ForsanNemah <forsan20172017@gmail.com>
2023-01-07 16:44:25 +0300

    add hokail

---
+++ hokeel/yezeed/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966598123444";
+var msg="تم التسجيل بنجاح ";
+var user_id="2";
+var action_url="https://script.google.com/macros/s/AKfycbxZkAr-FrS9taLz3EB-OlNLvk7liHhs_QGhPXb4PDB2zCzE8fR5oMLQWQJYhShkPbyl/exec";
+//alert(phn);
+
+
+
+
+
+
+
+ 
