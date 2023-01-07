fd7640a26a10f1b9f0c0bd0ee05c60fdc7215193
ForsanNemah <forsan20172017@gmail.com>
2023-01-07 17:56:37 +0300

    Update info.js

--- hokeel/ashraf/info.js
+++ hokeel/ashraf/info.js
@@ -4,7 +4,7 @@
 var phn="966598123444";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
-var action_url="https://script.google.com/macros/s/AKfycbxkC3aoN6-tYe1WbpSbaJaE0IAoh2XlX8gE7aa0f6nhZgMHCcF5F5pxn557j4-gtORB/exec";
+var action_url="https://script.google.com/macros/s/AKfycbxJF67k_ZBeAI2d_hvpii92ilR3SxX8w2VSISVQEYAu0gevHl3-BkmgMqcSt529H5oz/exec";
 //alert(phn);
 
 
