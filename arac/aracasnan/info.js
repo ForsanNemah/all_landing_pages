895198c1daa99d1d5773c857d2fd0872a5b07201
ForsanNemah <forsan20172017@gmail.com>
2023-01-08 22:31:26 +0300

    okk

--- arac/aracasnan/info.js
+++ arac/aracasnan/info.js
@@ -1,7 +1,7 @@
  
 
 
-var phn="9629074224";
+var phn="96290742244";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
 var action_url="https://script.google.com/macros/s/AKfycbzo9AuYO6Agm2tDsK5xHnHBaRpybnDlul_3MMfRyotvjjalhXvJqKQsistw1S5qwjmK/exec";
--- arac/araclazer/info.js
+++ arac/araclazer/info.js
@@ -2,7 +2,7 @@
 
 
 
-var phn="9629074224";
+var phn="96290742244";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
 var action_url="https://script.google.com/macros/s/AKfycbzLGD4Pg1sj-V0xni2Yq9cyYWffpyuU2wq6iqWH8W8rEb1bSuMj2TWkBf0gtFRC1GcXeg/exec";
