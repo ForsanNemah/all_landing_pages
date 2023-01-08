b72a2a406b19dc2b88a329b46f717b4675144dc8
ForsanNemah <forsan20172017@gmail.com>
2023-01-08 22:41:55 +0300

    add phn to arac

--- arac/aracskin/info.js
+++ arac/aracskin/info.js
@@ -2,7 +2,7 @@
 
 
 
-var phn="96290742244";
+var phn="96890742244";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
 var action_url="https://script.google.com/macros/s/AKfycbxH8rgZx3S3AZlGHsmN5xpSTaJWxZnT0VpokiBKSf8SIwwjo2e6Va-fHQO088R_qcEctQ/exec";
