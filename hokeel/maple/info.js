0f84a40fce185022a6cde1e89da45d39898b7c9f
ForsanNemah <forsan20172017@gmail.com>
2023-01-12 13:00:00 +0300

    add miple

---
+++ hokeel/maple/info.js
@@ -0,0 +1,17 @@
+ 
+
+
+var phn="966590990966";
+var msg="تم التسجيل بنجاح ";
+var user_id="2";
+var end_date_by_me="1 15, 2023 12:00:00";
+var action_url="https://script.google.com/macros/s/AKfycbwYeO7Ezy8y8jCdXSUcd6SnhpBXrYnVaOLto1UBmqUv1ZC-NQQrRTI2rrt3u2zNH1S8/exec";
+//alert(phn);
+
+
+
+
+
+
+
+ 
