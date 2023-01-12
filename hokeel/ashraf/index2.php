2bf22f52c69301570ba9ac79a350fe95d4db1019
ForsanNemah <forsan20172017@gmail.com>
2023-01-12 12:14:41 +0300

    add index 2

--- hokeel/ashraf/index2.php
+++ hokeel/ashraf/index2.php
@@ -4,7 +4,7 @@
     <head>
 
  
-
+ 
 
 
         <meta charset="utf-8">
@@ -36,6 +36,9 @@
         <link rel="stylesheet" href="assets/css/animate.css">
         <link rel="stylesheet" href="assets/css/style.css">
         <link rel="stylesheet" href="assets/css/media-queries.css">
+        
+       
+        
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -48,27 +51,52 @@
        
 		<script type="text/javascript" src="info.js"></script>
  
+   
+    
+
+
+
+
+
+
+
+
+    
+ 
+
+   
 
     </head>
 
     <body  dir="rtl">
 		
 		<!-- Top menu -->
-		 
+ 
+     
+    
 
 
 
 
 
 		<div class="row">
-					<div class="col-sm-8 col-sm-offset-2 text">
+					<div  >
 
 
 
-					<img   src="ads/logo.png" class="img-fluid" alt="Responsive image" width="120px" height="100px" >
+					<img   src="ads/logo.png" class="img-fluid" alt="Responsive image" width="100px" height="100px" >
 
 
-						<h1 class="wow fadeInLeftBig"> 
+         
+        
+
+
+
+
+
+
+
+						<h2 > 
 
 
 							<?php
@@ -84,9 +112,9 @@
 
 
 
-						</h1>
-						<div class="description wow fadeInLeftBig">
-							<p>
+						</h2>
+						<div >
+							<h3>
 								 
 							<?php
 
@@ -101,7 +129,17 @@ include 'info.php';
 
 
 
-							</p>
+							</h3>
+
+           <div id="count_down_counter">
+
+
+
+           </div>
+ 
+<br>
+
+
 						</div>
 					</div>
 				</div>
@@ -118,7 +156,9 @@ include 'info.php';
 
 
 
+  
 
+       
 
 
 
@@ -133,11 +173,11 @@ include 'info.php';
 
 				<div class="row">
 					<div class="col-sm-6 col-sm-offset-3 r-form-1-box wow fadeInUp">
-						<div class="r-form-1-top">
+						<div  >
 
 						
-							<div class="r-form-1-top-right">
-								<h3>
+							<div >
+								<h4>
 
 
 
@@ -160,8 +200,8 @@ include 'info.php';
 
 
 
-								</h3>
-								<p>
+								</h4>
+								<h>
 
 
 
@@ -178,112 +218,48 @@ include 'info.php';
 
 
 
-								</p>
+		</h>
 							</div>
 							 
 						</div>
+
+
+            
+
+
+
+
+
+
+
+            
 						<div class="r-form-1-bottom">
-							<form role="form"      method="post" name="myform">
 
-								<div class="form-group">
-									<label class="sr-only" for="r-form-1-first-name"> Name</label>
-									<input type="text" name="name"   placeholder="الأسم" class="r-form-1-first-name form-control" id="r-form-1-first-name"  required>
-								</div>
-								 
-								 
+            <div class="col-xs-4 col-xs-offset-4" id="myloader" hidden >
+<div class="loader"></div> 
+</div>
 
+<div class="col-xs-4 col-xs-offset-4"  id="done_icon" hidden>
+<img src="ads/done.gif" class="img-fluid" alt="Responsive image"   >
 
-								<div class="form-group" >
-									<label class="sr-only" for="r-form-1-email">Email</label>
-									<input type="number" name="phn" placeholder="رقم الهاتف " class="r-form-1-email form-control" id="r-form-1-email" required>
-								</div>
+</div>
 
+<br>
+<br>
+<br>
+<br>
+            <form id="includedContent" role="form"      method="post" name="myform">
 
-								<div class="form-group" hidden >
-									<label class="sr-only" for="r-form-1-email">date</label>
-									<input type="text" name="datetime" id="today" placeholder=" التاريخ  " class="r-form-1-email form-control" id="r-form-1-email"  >
-								</div>
-
-
-								<!--
-
-								
-
-
-								<div class="form-group" dir="rtl">
-                                          <select name="type" id="servicerb" class="custom-select" aria-label="عرض زراعة الأسنان" required="true">
-
-                                             
-
-
-
-                                            <?php
-
-include 'info.php';
-
- 
-
-
-foreach ($items as $key => $value) {
-
-    echo "<option value='$value'>$value</option>";
+          
      
-   }
-?>
-                                        </select>
-                                    </div>
-								 
+            </form>
+            
+       
 
--->
+            
 
 
-
-
-
-
-				 
-
-
-
-
-
-
-
-
-
-
-
-
-<br>
-<br>
-<button type="submit"  class="btn submit-btn" style="background-color: #25D366"> <i >
-
-
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-	<path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
-  </svg>
-
-
-
-</i> احجز  عبر واتس اب </button>
-
-
-
-
-
-
-
-
-
-
-
-
-							</form>
-
-
-
-
-
+            
 
 
 
@@ -310,43 +286,39 @@ foreach ($items as $key => $value) {
 
 		<div class="how-it-works-container section-container section-container-gray-bg  "    >
 	        <div class="container">
+
+         
 	            <div class="row">
-	                 
 
 
+               
 
+<div id="slider1"></div>
+           
 
- 
-
-
-
-
-
-
+  </div>	 
 <br>
 
+  <div class="row">
+	                 
+
+                   <div id="slider2"></div>
+                              
+                   
+                     </div>	 
 
 
-  
+                     <br>
 
+  <div class="row">
+	                 
 
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
+                   <div id="slider3"></div>
+                              
+                   
+                     </div>	 
+                   
+                   
 
 
 
@@ -449,7 +421,15 @@ foreach ($items as $key => $value) {
        
         
         <!-- Footer -->
-         
+		<footer class="page-footer font-small blue">
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2022 جميع الحقوق محفوظة:
+  <a href="https://wmc-ksa.com/"> شركة النافذة</a>
+</div>
+<!-- Copyright -->
+
+</footer>
 		 
         <!-- Javascript -->
         <script src="assets/js/jquery-1.11.1.min.js"></script>
@@ -484,12 +464,57 @@ foreach ($items as $key => $value) {
 
 <script>
 
-
-
-
-
-
+ 
 $(document).ready(function () {
+
+
+
+
+
+  
+
+//alert($('#if_cd').contentWindow.document.body.offsetHeight + 'px');
+
+//$('#if_cd').height("200" );
+
+/*
+$('#if_cd').load(function() {
+    this.style.height =
+    this.contentWindow.document.body.offsetHeight + 'px';
+    alert( this.contentWindow.document.body.offsetHeight + 'px');
+    $('#if_cd').height("146" );
+});
+*/
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
 
 	var currentdate = new Date(); 
 var datetime = "" + currentdate.getDate() + "/"
@@ -498,11 +523,59 @@ var datetime = "" + currentdate.getDate() + "/"
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
+                //alert(datetime);
 
 
+                $("#includedContent").load("form_name_phn_fixed_type.php"); 
+                $("#slider1").load("slider1.php"); 
+               // $("#slider2").load("slider2.php"); 
+                $("#count_down_counter").load("count_down.php"); 
+                //$("#slider3").load("slider3.php"); 
+                
+               // $("#includedContent").load("form_name_phn.php"); 
+                // $("#includedContent").load("form_only_whatsapp.php"); 
+				
+ 
+    
 
-				$('#today').attr('value', datetime );
+      const scriptURL = action_url
+  const form = document.forms['myform']
 
+  form.addEventListener('submit', e => {
+    
+	 
+	$(':input[type=submit]').prop('disabled', true)
+  $('#today').attr('value', datetime )
+  $("#myloader").show()
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => 
+ 
+      
+      google_response()
+      
+      
+     
+      
+      
+    
+   
+    
+	
+    
+	  )
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+    
+  function google_response() {
+    $("#myloader").hide();
+    $("#done_icon").show();
+     
+    window.location.replace("https://wa.me/"+phn) ;
+    
+}
 				 
 	
 });
@@ -523,25 +596,6 @@ var datetime = "" + currentdate.getDate() + "/"
 
 
 
-const scriptURL = action_url
-  const form = document.forms['myform']
-
-  form.addEventListener('submit', e => {
-
-	 
-	//window.location.replace("done.html");
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => 
-	  
-	  window.location.replace("https://wa.me/"+phn)
-	  
-	  
-	 
-	  )
-      .catch(error => console.error('Error!', error.message))
-  })
-
 
 
 
@@ -556,7 +610,39 @@ const scriptURL = action_url
 <style>
 
 
+select {
+        height:35px;
+        line-height:30px;
+        background:#f4f4f4;
+        
+    } 
 
+
+
+
+    .loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
  
+ 
+
+
  
 </style>
