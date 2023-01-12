73d7d6ab7f4c806e128c948db55c8b244db9f41b
ForsanNemah <forsan20172017@gmail.com>
2023-01-12 13:49:19 +0300

    Update index.php

--- nass/index.php
+++ nass/index.php
@@ -528,7 +528,7 @@ var datetime = "" + currentdate.getDate() + "/"
 
                 $("#includedContent").load("form_name_phn_type.php"); 
                 $("#slider1").load("slider1.php"); 
-                $("#slider2").load("slider2.php"); 
+                //$("#slider2").load("slider2.php"); 
                 $("#count_down_counter").load("count_down.php"); 
                 //$("#slider3").load("slider3.php"); 
                 
