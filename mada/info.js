2ce28a954881a68a01f3077e30f53cea3c2bea70
ForsanNemah <forsan20172017@gmail.com>
2023-01-14 17:47:52 +0300

    add counter to mada

--- mada/info.js
+++ mada/info.js
@@ -5,6 +5,7 @@
 var phn="966566634800";
 var msg="تم التسجيل بنجاح ";
 var user_id="2";
+var end_date_by_me="1 15, 2023 12:00:00";
 var action_url="https://script.google.com/macros/s/AKfycbxZWklJd6QrnlFQlim7BAIj0Wj7zbiK1TyWv43I3mWhKxO51nnmvka0rjQcfqHzdoAL/exec";
 
  
