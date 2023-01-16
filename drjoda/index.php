0190eaa4598e0c5c3cf90aac60c6abcfb12b4c3e
ForsanNemah <forsan20172017@gmail.com>
2023-01-16 14:21:21 +0300

    Update index.php

--- drjoda/index.php
+++ drjoda/index.php
@@ -74,7 +74,7 @@
 
 
 
-					<img   src="ads/logo.png" class="img-fluid" alt="Responsive image" width="300px" height="300px" >
+					<img   src="ads/logo.png" class="img-fluid" alt="Responsive image" width="200px" height="200px" >
 
 
 						<h2 > 
