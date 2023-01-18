6737939e5995f9451b0abaa7a92c0a6d1c8a02a9
ForsanNemah <forsan20172017@gmail.com>
2023-01-18 12:25:06 +0300

    s

--- prozoon/prozoon2/info.js
+++ prozoon/prozoon2/info.js
@@ -5,7 +5,7 @@ var phn="966530442101";
 var msg="تم التسجيل بنجاح ";
 var end_date="25-1-2023";
 var end_time="12:00";
-var action_url="https://script.google.com/macros/s/AKfycbz5Z5PpfDC0LkNAbkruFJyqpmdQLQ5Rt8AdovvdpnFrBwOCGkYbGL-9RA53EiGl9stJkg/exec";
+var action_url="https://script.google.com/macros/s/AKfycbxxURgwMK04zuY1RdxmWplzvteYdpqNYmqf00hRDwXaGFxuB5JZcYW3zGZObJgxNRfaLQ/exec";
 
 
 
