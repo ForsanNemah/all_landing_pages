643feadd7750efaddd8540a0ba168a3b6104075b
ForsanNemah <forsan20172017@gmail.com>
2023-01-19 10:39:17 +0300

    final ethoria

--- ephoria/count_down.php
+++ ephoria/count_down.php
@@ -1,4 +1,4 @@
-<div style="padding-bottom:25.25%; position:relative; display:block; width: 100%">
+<div style="padding-bottom:12%; position:relative; display:block; width: 100%">
   <iframe width="100%" height="100%"
     src="count_down/index.html"
     frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0">
--- ephoria/ephoria2/count_down.php
+++ ephoria/ephoria2/count_down.php
@@ -1,5 +1,5 @@
-<div style="padding-bottom:25.25%; position:relative; display:block; width: 100%">
-  <iframe width="100%" height="50%"
+<div style="padding-bottom:12%; position:relative; display:block; width: 100%">
+  <iframe width="100%" height="100%"
     src="count_down/index.html"
     frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0">
   </iframe>
