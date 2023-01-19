43db976d0039cc9f0705a7a7f8369738b1c591a8
ForsanNemah <forsan20172017@gmail.com>
2023-01-19 17:33:12 +0300

    done ephoria

--- ephoria/info.js
+++ ephoria/info.js
@@ -3,7 +3,7 @@
 
 var phn="966500663318";
 var msg="تم التسجيل بنجاح ";
-var end_date="20-1-2023";
+var end_date="25-1-2023";
 var end_time="12:00";
 var action_url="https://script.google.com/macros/s/AKfycbzc8kmhZMGI8CpXIsyJOuN9RFNuURFjYvBUGhyj_bSVZ4xAmCQDPPhH606X4Nz4wyoFhg/exec";
 
