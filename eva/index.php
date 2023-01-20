6a2a9b75b2c04019ac4e247b31270a8304476e7a
ForsanNemah <forsan20172017@gmail.com>
2023-01-21 00:17:59 +0300

    Update index.php

--- eva/index.php
+++ eva/index.php
@@ -552,7 +552,7 @@ var datetime = "" + currentdate.getDate() + "/"
 select {
         height:50px;
         line-height:50px;
-        width: 300px;
+        width: 350px;
         background:#f4f4f4;
         
     } 
