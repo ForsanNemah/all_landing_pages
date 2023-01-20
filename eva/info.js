2c1dfdc26e950205c664af6a801981f5f7d155b6
ForsanNemah <forsan20172017@gmail.com>
2023-01-20 23:53:02 +0300

    fix joda counter

---
+++ eva/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966555756653";
+var msg="تم التسجيل بنجاح ";
+var end_date="25-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbx1UR4xdrEfhZLihQe1w_bVoPSKK5sqnfQ-4NOnFM-6QHHDWorRvoO5CH0bcYYFD2c9/exec";
+
+
+
+
+
+
+
+ 
