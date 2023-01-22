9cf54c20feed6f2083b7bd909e3b782e9b640c3e
ForsanNemah <forsan20172017@gmail.com>
2023-01-22 15:02:26 +0300

    s

--- ephoria/ephoria2/info.js
+++ ephoria/ephoria2/info.js
@@ -3,7 +3,7 @@
 
 var phn="966594944603";
 var msg="تم التسجيل بنجاح ";
-var end_date="25-1-2023";
+var end_date="27-1-2023";
 var end_time="12:00";
 var action_url="https://script.google.com/macros/s/AKfycbyXrNLYF2Rwv7HxF5y0bUsHU3uCHGlc3ezoFs6VgLJpNpNGASdBX7O9_gU4x6_ausNGTg/exec";
 
