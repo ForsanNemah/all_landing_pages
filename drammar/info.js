0a4cc3df1af34b46462104dfbc68e400d697fcac
ForsanNemah <forsan20172017@gmail.com>
2023-01-23 12:45:47 +0300

    dr ammar royal

---
+++ drammar/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966540915499";
+var msg="تم التسجيل بنجاح ";
+var end_date="29-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbzwANoHl4m_ynPclSXuICbUeTeSuXdttsyaLUKznT3VHIH6rsWFE01Ls3oSCmLrGbdu2Q/exec";
+
+
+
+
+
+
+
+ 
