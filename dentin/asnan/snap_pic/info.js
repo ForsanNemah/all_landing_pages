0e6173de587a21d5dbfffaff32fa97133211d877
ForsanNemah <forsan20172017@gmail.com>
2023-01-24 12:50:50 +0300

    add final dentin

---
+++ dentin/asnan/snap_pic/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966534206222";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="snap chat pic";
+var action_url="https://script.google.com/macros/s/AKfycbz8iSmeENfGlIsKkirpSmPM9s0t4cZBbliJyGYNx8Yhav-XAYyGX9CoF2ro_cdlT5sR/exec";
+
+
+
+
+
+
+
+ 
---
+++ dentin/asnan/snap_vid/info.php
@@ -0,0 +1,94 @@
+<?php
+
+
+$items = array();
+
+
+
+$handle = fopen("services/asnan_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+
+    "website_title"=>"    عيادات دنتين Dentin clinic",
+     "sub1"=>"         عيادات دنتين Dentin clinic   ",
+    "sub2"=>" جلدية | ليزر | تجميل | كافة تخصصات طب الأسنان     ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
