d1d64af7c54de0ecace03111b6603d21026e7488
ForsanNemah <forsan20172017@gmail.com>
2023-01-26 16:05:35 +0300

    add liberty

---
+++ libert/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966581416355";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="snap";
+var action_url="https://script.google.com/macros/s/AKfycbx9DDyG48Tln5p-TIdX8Zbgrmi3F31b88-FEnJZVexRtpIgZCkmiCgoThzflMM1WPTF/exec";
+
+
+
+
+
+
+
+ 
