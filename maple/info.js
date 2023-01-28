1bd81c3ba3d9b917f8960d78c832686da0291338
ForsanNemah <forsan20172017@gmail.com>
2023-01-28 18:25:39 +0300

    add maple

---
+++ maple/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966590990966";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="  1   ";
+var action_url="https://script.google.com/macros/s/AKfycbwYeO7Ezy8y8jCdXSUcd6SnhpBXrYnVaOLto1UBmqUv1ZC-NQQrRTI2rrt3u2zNH1S8/exec";
+
+
+
+
+
+
+
+ 
---
+++ maple/pixel/snap.js
@@ -0,0 +1,15 @@
+ 
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+
+snaptr('init', 'a63d6c92-8480-46c6-be36-5562b590b9c3', {
+'user_email': '_INSERT_USER_EMAIL_'
+});
+
+snaptr('track', 'PAGE_VIEW');
+
+ //alert("end pixel");
