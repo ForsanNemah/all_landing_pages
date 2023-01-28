61f73977f0b6cb7ef2e14ea4ff0e445031573821
ForsanNemah <forsan20172017@gmail.com>
2023-01-28 16:17:44 +0300

    aljameel

---
+++ aljameel/aljameel2/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966563605558";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source=" من المشهور ";
+var action_url="https://script.google.com/macros/s/AKfycbyAKMX8VbaePcbTNcDRStPjPQHxV2ZXc_-IR3_gJhzHddWxDSSrdNlKCfOFov-Tfy8/exec";
+
+
+
+
+
+
+
+ 
