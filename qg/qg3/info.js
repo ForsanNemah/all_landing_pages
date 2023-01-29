4ed9a5217a475160ea70904ba897d23a18fad015
ForsanNemah <forsan20172017@gmail.com>
2023-01-29 12:26:51 +0300

    add qg lazer

---
+++ qg/qg3/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966552557304";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="من الاعلان ";
+var action_url="https://script.google.com/macros/s/AKfycbwI8a45C6hV5eaLi01Z1wUo9_X_E8a91lxBLpneL0LBjn9na8i-tA1tIhJ0g3s15JKv/exec";
+
+
+
+
+
+
+
+ 
