b0a54483f5e8d8285a1256628d69fc8a47841e48
ForsanNemah <forsan20172017@gmail.com>
2023-01-31 00:35:12 +0300

    addd

---
+++ eva/eva/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966555756653";
+var msg="تم التسجيل بنجاح ";
+var end_date="25-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbzrD-ATu7lfN8Ihr15F162ZSflbBcB7zpe-hnIY4Whw-FNN4hs-YZ9_mdN4l4GhWzdI/exec";
+
+
+
+
+
+
+
+ 
---
+++ eva/eva/info.php
@@ -0,0 +1,94 @@
+<?php
+
+
+$items = array();
+
+
+
+$handle = fopen("services/eva_lazer_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+
+    "website_title"=>"  عيادات إيڤا",
+     "sub1"=>"        عيادات إيڤا      ",
+    "sub2"=>"عروض بداية العام     ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
