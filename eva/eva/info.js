1417767fc78974af8832ba2c94ee879ae8df86c9
ForsanNemah <forsan20172017@gmail.com>
2023-02-01 02:06:47 +0300

    fix eva

--- eva/eva/info.js
+++ eva/eva/info.js
@@ -3,7 +3,7 @@
 
 var phn="966555076683";
 var msg="تم التسجيل بنجاح ";
-var end_date="25-1-2023";
+var end_date="25-2-2023";
 var end_time="12:00";
 var action_url="https://script.google.com/macros/s/AKfycbzrD-ATu7lfN8Ihr15F162ZSflbBcB7zpe-hnIY4Whw-FNN4hs-YZ9_mdN4l4GhWzdI/exec";
 
---
+++ eva/eva3/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966555076683";
+var msg="تم التسجيل بنجاح ";
+var end_date="25-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbzYBMA_vxagIKKSghbgBpJcwo8TE5L0JpbKBEoeSegaOmg2XJAo0GmLqQ65wrG-2HwF/exec";
+
+
+
+
+
+
+
+ 
