7cfcef40016697de6ba22129014639d34e9fec41
ForsanNemah <forsan20172017@gmail.com>
2023-02-01 02:59:00 +0300

    qg

--- eva/eva3/info.js
+++ eva/eva3/info.js
@@ -1,9 +1,9 @@
  
 
 
-var phn="966555076683";
+var phn="966505506913";
 var msg="تم التسجيل بنجاح ";
-var end_date="25-1-2023";
+var end_date="5-2-2023";
 var end_time="12:00";
 var action_url="https://script.google.com/macros/s/AKfycbzYBMA_vxagIKKSghbgBpJcwo8TE5L0JpbKBEoeSegaOmg2XJAo0GmLqQ65wrG-2HwF/exec";
 
--- greeny/info.js
+++ greeny/info.js
@@ -2,7 +2,7 @@
 
 
 var phn="966564416049";
-var end_date="5-2-2023";
+var end_date="1-5-2022";
 var end_time="12:00";
 var ad_source="من الاعلان ";
 var action_url="https://script.google.com/macros/s/AKfycbxCzRksqHp7VjZpUdktixFR8JlVl0iO9gfnoZ9Ew46cp8bMemy3GoIxin50YFYsjHELvw/exec";
