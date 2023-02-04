f5b2f245a06495bd88e06694169d3a2818cd7e31
ForsanNemah <forsan20172017@gmail.com>
2023-02-04 15:07:24 +0300

    final greeny

--- greeny/info.php
+++ greeny/info.php
@@ -5,7 +5,7 @@ $items = array();
 
 
 
-$handle = fopen("services/asnan_services.txt", "r");
+$handle = fopen("services/greeny_services.txt", "r");
 if ($handle) {
 $i=0;
     while (($line = fgets($handle)) !== false) {
