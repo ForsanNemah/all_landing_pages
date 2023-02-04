babd18f27ae5bede7972e76a9e227dbdeec9eb0f
ForsanNemah <forsan20172017@gmail.com>
2023-02-04 15:32:24 +0300

    final liber t

--- libert/info.js
+++ libert/info.js
@@ -2,7 +2,7 @@
 
 
 var phn="966583200545";
-var end_date="29-1-2023";
+var end_date="10-2-2023";
 var end_time="12:00";
 var ad_source="snap";
 var action_url="https://script.google.com/macros/s/AKfycbx9DDyG48Tln5p-TIdX8Zbgrmi3F31b88-FEnJZVexRtpIgZCkmiCgoThzflMM1WPTF/exec";
