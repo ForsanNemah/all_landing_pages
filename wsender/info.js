a92e795d67ccb60d4fce49997bfef72e21dd66d3
ForsanNemah <forsan20172017@gmail.com>
2023-02-04 04:38:06 +0300

    add wenser

---
+++ wsender/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966553907575";
+var end_date="1-5-2022";
+var end_time="12:00";
+var ad_source=" 1   ";
+var action_url="https://script.google.com/macros/s/AKfycbxeojzKBLKHTr0ssDsg-0Rdu6R96NQNEzPEm97lQ3KmxMw2z1mAFyx92a08qHS1CsIZ/exec";
+
+
+
+
+
+
+ 
