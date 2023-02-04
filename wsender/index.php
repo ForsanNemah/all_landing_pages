71fcf48626d22b5e943fb575efbab96e484748b6
ForsanNemah <forsan20172017@gmail.com>
2023-02-04 04:43:07 +0300

    Update index.php

--- wsender/index.php
+++ wsender/index.php
@@ -47,8 +47,12 @@
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <script type="text/javascript" src="pixel/snap.js"></script>
-        <!-- Favicon and touch icons -->
+      
+        <!-- 
+          
+          <script type="text/javascript" src="pixel/snap.js"></script>
+        
+        Favicon and touch icons -->
        
 		<script type="text/javascript" src="info.js"></script>
  
