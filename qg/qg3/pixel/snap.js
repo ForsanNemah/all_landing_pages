29f7001141ad22dc01ea7837c0b98f28754fa411
ForsanNemah <forsan20172017@gmail.com>
2023-02-05 13:13:28 +0300

    add pixcel

--- qg/qg3/pixel/snap.js
+++ qg/qg3/pixel/snap.js
@@ -1,4 +1,4 @@
- 
+<script type='text/javascript'>
 (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
 {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
 a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
@@ -6,10 +6,8 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'c530b9a9-f8d4-48df-ba97-3958aae4f9a6', {
+snaptr('init', '3a848c55-5abb-4d20-9e5c-80a882636a52', {
 'user_email': '_INSERT_USER_EMAIL_'
 });
 
 snaptr('track', 'PAGE_VIEW');
- 
-<!-- End Snap Pixel Code -->
