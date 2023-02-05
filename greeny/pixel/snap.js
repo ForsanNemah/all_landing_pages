5aeee019e548cc303877ebe565e82a29add62a44
ForsanNemah <forsan20172017@gmail.com>
2023-02-05 13:02:15 +0300

    Update snap.js

--- greeny/pixel/snap.js
+++ greeny/pixel/snap.js
@@ -1,4 +1,4 @@
- 
+
 (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
 {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
 a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
@@ -6,10 +6,11 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'c530b9a9-f8d4-48df-ba97-3958aae4f9a6', {
+snaptr('init', 'b0c8aed0-7f15-45ec-a70b-14104cbb0cce', {
 'user_email': '_INSERT_USER_EMAIL_'
 });
 
 snaptr('track', 'PAGE_VIEW');
- 
-<!-- End Snap Pixel Code -->
+
+ //alert("wwe ");
+
