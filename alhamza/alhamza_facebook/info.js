035b98542bfa2b4c635e0396fabccd7704cad7b7
ForsanNemah <forsan20172017@gmail.com>
2023-02-08 13:42:07 +0300

    add alhamza facebook

--- alhamza/alhamza_facebook/info.js
+++ alhamza/alhamza_facebook/info.js
@@ -4,7 +4,7 @@
 var phn="966505847810";
 var end_date="1-5-2022";
 var end_time="12:00";
-var ad_source="snap   ";
+var ad_source=" faecbook ";
 var action_url="https://script.google.com/macros/s/AKfycbwtTttyHgx7KGTB4LUkm-PX23cEMzln3XOwq9JilUq1zDDuv7oYbISA9oLcof7xfkBE3Q/exec";
 
 
---
+++ alhamza/alhamza_facebook/info.php
@@ -0,0 +1,102 @@
+<?php
+
+
+$items = array();
+
+
+
+$handle = fopen("services/alhamza_cars.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+    
+
+    "website_title"=>"  الحمزة للسيارات",
+     "sub1"=>"      الحمزة للسيارات  ",
+    "sub2"=>"أفضل عروض شانجان - شانجان جديدة بأفضل سعر   
+      ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>"  اشترى احدث موديلات شانجان         ",
+ "sidebar2_msg"=>"  تجهيزات باحدث التقنيات     ",
+ "sidebar3_msg"=>"     2  كادر طبي متميز       ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
