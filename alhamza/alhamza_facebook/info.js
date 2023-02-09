ee804e83610d5c9987daa50545a467825721829e
ForsanNemah <forsan20172017@gmail.com>
2023-02-09 12:53:06 +0300

    Update info.js

--- alhamza/alhamza_facebook/info.js
+++ alhamza/alhamza_facebook/info.js
@@ -5,7 +5,7 @@ var phn="966505847810";
 var end_date="1-5-2022";
 var end_time="12:00";
 var ad_source=" faecbook ";
-var action_url="https://script.google.com/macros/s/AKfycbwtTttyHgx7KGTB4LUkm-PX23cEMzln3XOwq9JilUq1zDDuv7oYbISA9oLcof7xfkBE3Q/exec";
+var action_url="https://script.google.com/macros/s/AKfycby0bYOiA84Ius3eoz8FNlp1LKigXTgO-zEz9JSILdqvJVgeadKqCBOsybbG_u8Ftzko/exec";
 
 
 
