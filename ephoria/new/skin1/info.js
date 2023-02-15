c16638731a5d3e437244419d8d276aa3c5c9dd36
ForsanNemah <forsan20172017@gmail.com>
2023-02-15 12:11:14 +0300

    add 3 evthoria

---
+++ ephoria/new/skin1/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966500663318";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbzWJRJTlxdRNTMcwfRs0X8MlEY1-NJJ6ULW0i6om21U6Y5QHRjg6-ePHeORQlCydAcz/exec";
+
+
+
+
+
+
+
+ 
---
+++ ephoria/new/skin2/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966500663318";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbzc8kmhZMGI8CpXIsyJOuN9RFNuURFjYvBUGhyj_bSVZ4xAmCQDPPhH606X4Nz4wyoFhg/exec";
+
+
+
+
+
+
+
+ 
