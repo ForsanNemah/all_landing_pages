1709426ad1cda3b7fd12cb4ad8ede9bf6c87e76e
ForsanNemah <forsan20172017@gmail.com>
2023-02-15 14:10:15 +0300

    add

--- ephoria/new/skin3/index.php
+++ ephoria/new/skin3/index.php
@@ -80,7 +80,7 @@
 
 
 
-					<img   src="ads/logo.png" class="img-fluid" alt="Responsive image" width="200px" height="200px" >
+					<img   src="ads/logo.png" class="img-fluid" alt="Responsive image" width="180px" height="150px" >
 
 
 						<h2 > 
