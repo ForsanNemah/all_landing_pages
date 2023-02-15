473f45042b5febc561215162889e7937d3ea2506
ForsanNemah <forsan20172017@gmail.com>
2023-02-15 13:48:40 +0300

    add

--- ephoria/new/skin2/info.js
+++ ephoria/new/skin2/info.js
@@ -5,7 +5,7 @@ var phn="966500663318";
 var msg="تم التسجيل بنجاح ";
 var end_date="27-1-2023";
 var end_time="12:00";
-var action_url="https://script.google.com/macros/s/AKfycbzc8kmhZMGI8CpXIsyJOuN9RFNuURFjYvBUGhyj_bSVZ4xAmCQDPPhH606X4Nz4wyoFhg/exec";
+var action_url="https://script.google.com/macros/s/AKfycbyFzHOgr5CFvnWTAHeixutIAaIIZAq3Oau78XqTqCCsOAlWHiuqNZTe5_ISKAQxU9WZQg/exec";
 
 
 
--- ephoria/new/skin3/info.js
+++ ephoria/new/skin3/info.js
@@ -5,7 +5,7 @@ var phn="966500663318";
 var msg="تم التسجيل بنجاح ";
 var end_date="27-1-2023";
 var end_time="12:00";
-var action_url="https://script.google.com/macros/s/AKfycbzc8kmhZMGI8CpXIsyJOuN9RFNuURFjYvBUGhyj_bSVZ4xAmCQDPPhH606X4Nz4wyoFhg/exec";
+var action_url="https://script.google.com/macros/s/AKfycbxLJfYjxGPLt_oNW6Dirjj62CF-Q0BXMcacUptsq5LlBMsgH-dJVi_YHjV9dBvn3X3dew/exec";
 
 
 
