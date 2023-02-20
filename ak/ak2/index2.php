83e1a07b58aa7ab50d956fa55ac7384b20758d77
ForsanNemah <forsan20172017@gmail.com>
2023-02-20 04:11:50 +0300

    ad ak 2

---
+++ ak/ak2/index2.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="title" content="">
+    <meta property="description" content=" ">
+    <meta property="url" content=" ">
+    <meta property="site_name" content=" ">
+    <meta property="og:type" content="">
+    <meta property="og:title" content=" ">
+    <meta property="og:description" content=" ">
+    <meta property="og:url" content=" ">
+    <meta property="og:site_name" content=" ">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="public/image/favicon.png" type="image/gif" sizes="16x16">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>Meswak Clinics</title>
+    <style>
+        /* WhatsApp Button */
+
+        .whatsApp {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 10px;
+            background-color: #25d366;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
+
+        .my-whatsApp {
+            margin-top: 9px;
+        }
+    </style>
+    <!-- Google Tag Manager -->
+    <script>
+
+
+      </script>
+    <!-- End Google Tag Manager -->
+
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    
+    <!-- End Google Tag Manager (noscript) -->
+    <div>
+        <span>
+            <a id="wsmalt" class="btn btn-primary whatsApp" style="background-color: #25D366" href="https://meswakrydnahdah.everestsoftjo.com/whatsapp.html"> <i class="fa fa-whatsapp  my-whatsApp" aria-hidden="true"></i> </a>
+        </span>
+    </div>
+    <!-- Header -->
+    <section class="container-fluid bg-primary head-nav">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6">
+                <div class="text-center header-logo">
+                    <div class="logo">
+                        <img src="public/image/Logo.png" alt="logo">
+                        <h3 class="text-light" style="font-size:1.2em!important">أفضل رعاية طبية نقدمها لكم</h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Appointment Section -->
+    <section class=" appointment">
+        <div class="appointment-section">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-12 col-lg-6">
+                        <div class="form">
+                            <div class="appointment-form text-center">
+                                <form id="form" target="_self" onsubmit="return postToGoogle();" action="" autocomplete="off">
+                                    <p class="appointment-title text-light">املأ النموذج الآن</p>
+                                    <p class="appointment-title text-light" style="font-size:1.2em;direction:rtl">واحصل على العرض</p>
+                                    <div class="form-group">
+                                        <input id="nameField" type="text" class="form-control" placeholder="الاسم" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input id="mobField" type="number" class="form-control" placeholder="رقم الهاتف" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <select name="servicerb" id="servicerb" class="custom-select" aria-label="عرض زراعة الأسنان" required>
+                                            <option value="عرض زراعة الأسنان">اختر الخدمة</option>
+                                            <option value="عرض زراعة الأسنان">عرض زراعة الأسنان</option>
+                                            <option value="عرض إبتسامة هوليود">عرض إبتسامة هوليود</option>
+                                            <option value="عرض تركيبات الأسنان">عرض تركيبات الأسنان</option>
+                                            <option value="عرض المعالجات">عروض حشوات ومعالجات</option>
+                                            <option value="عرض تقويم الاسنان">عرض تقويم الأسنان</option>
+                                            <option value="عرض تبييض الاسنان">عرض تبييض الاسنان</option>
+                                            <option value="خدمات اخرى">عروض اخرى</option>
+                                        </select>
+                                    </div>
+
+                                    <button id="send" type="submit" class="btn submit-btn">اضغط للتسجيل</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 passion">
+                        <div class="text-center text-lg-right">
+                            <h4 class="text-light">
+                                عالج اسنانك او جمّلها فابتسامتك ستجعلك تبدو دائما مذهلا
+                            </h4>
+                            <br />
+                            <p class="text-light" style="font-size: 1.2em; font-size: 1.2em; font-family: inherit !important ">تتوفر خدمة التقسيط على دفعات</p>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="notification">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="blood-bank d-inline-block" style="text-align:center">
+                        <img src="public/image/doctors.png">
+                        <br>
+                        <h6 style="text-align: center; padding-top: 10px ">
+                            نخبة من أفضل الاستشاريين والاخصائيين
+                        </h6>
+                        <br> <span style="text-align: right; padding-top: 10px">نعتبر أنفسنا من الرواد في هذا المجال بأسماء أطبائنا الذين هم جزء لا يتجزأ من كياننا ومن الجسم الطبي في المملكة العربية السعودية</span>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="quality-medicine d-inline-block" style="text-align:center">
+                        <img src="public/image/dental.png">
+                        <br>
+                        <h6 style="text-align: center !important; padding-top: 10px">أجهزة متطورة ومواد طبية عالمية</h6>
+                        <br> <span style="text-align: right;">نستخدم افضل الأجهزة المتطورة في القطاع الطبي والمواد الطبية الافضل عالميا وذلك لنقدم خدمة طبية متكاملة </span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12 text-center">
+                    <a class="btn btn-primary" style="background-color: #1c3c49 " href="#form">  سجل الان </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="">© 2022 <a href="#form" class="copy">Meswak Clinics</a>.All rights reserved</p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+    <!-- <script src="js/jquery-3.4.1.min.js"></script> -->
+    <!-- <script src="js/main.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script src="public/custom.js"></script>
+    <script>
+
+        function hidebutton() {
+            $("#stickybutton").hide();
+        }
+
+        function postToGoogle() {
+
+            var field1 = $("#nameField").val();
+            var field2 = $("#mobField").val();
+            var field3 = $("#servicerb").val();
+
+            $.ajax({
+                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfTr6bgznjpemYAueQEWhNszAaf5gm0SpzNHRM5gGD3k5j9Ow/formResponse?",
+                data: { "entry.1150723794": field1, "entry.993982738": field2, "entry.581591335": field3 },
+                type: "POST",
+                dataType: "xml",
+                success: function (d) {
+                },
+                error: function (x, y, z) {
+
+                    window.location.replace("https://meswakrydnahdah.everestsoftjo.com/thankyou.html");
+
+
+
+                }
+            });
+            return false;
+        }
+    </script>
+</body>
+</html>
---
+++ ak/ak2/info.php
@@ -0,0 +1,110 @@
+<?php
+
+
+$items = array();
+
+
+
+$handle = fopen("services/ak_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+    
+
+    "website_title"=>"   AK SPA",
+     "sub1"=>"    
+       شريكك الموثوق
+       
+       به
+       
+       "
+       ,
+
+    "sub2"=>" 
+       عندما يتعلق الأمر بالاسترخاء والتدليك الاحترافي
+    ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>"  دلع نفسك معنا       ",
+ "sidebar2_msg"=>"  تجهيزات باحدث التقنيات     ",
+ "sidebar3_msg"=>"     2  كادر طبي متميز       ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
