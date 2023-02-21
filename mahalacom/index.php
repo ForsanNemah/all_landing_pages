5c3e396dfddf71f731e19369d12c701824ee39a2
ForsanNemah <forsan20172017@gmail.com>
2023-02-21 17:56:21 +0300

    Update index.php

--- mahalacom/index.php
+++ mahalacom/index.php
@@ -582,7 +582,7 @@ var datetime = "" + currentdate.getDate() + "/"
                 $("#includedContent").load("form_name_phn_type.php"); 
                 $("#slider1").load("slider1.php"); 
                 //$("#slider2").load("slider2.php"); 
-               // $("#cta_whatsapp").load("cta/cta_whatsapp.php"); 
+                $("#cta_whatsapp").load("cta/cta_whatsapp.php"); 
              // $("#cta_call").load("cta/cta_call.php"); 
                 //$("#slider3").load("slider3.php"); 
                 
