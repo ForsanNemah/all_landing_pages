4a541eb61b5a668d22d4745ba5e0ea7267478a9b
ForsanNemah <forsan20172017@gmail.com>
2023-02-21 11:16:58 +0300

    Update info.js

--- aljameel/info.js
+++ aljameel/info.js
@@ -5,7 +5,7 @@ var phn="966563615558";
 var end_date="29-1-2023";
 var end_time="12:00";
 var ad_source="من الاعلان ";
-var action_url="https://script.google.com/macros/s/AKfycbyAKMX8VbaePcbTNcDRStPjPQHxV2ZXc_-IR3_gJhzHddWxDSSrdNlKCfOFov-Tfy8/exec";
+var action_url="https://script.google.com/macros/s/AKfycbyx1krvzzhbMvCZpK5SWqMjiGlX7dK7mEH5LNk3QRo0SRW-WT3yyTpAk8YSj8RUWkiFuw/exec";
 
 
 
