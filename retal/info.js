af15e9fdf914a1d778ff0a692b8272b8543c2e08
ForsanNemah <forsan20172017@gmail.com>
2023-02-23 01:18:14 +0300

    add

--- retal/info.js
+++ retal/info.js
@@ -2,7 +2,7 @@
 
 
 var phn="966570041576";
-var end_date="1-5-2022";
+var end_date="32-2-2023";
 var end_time="12:00";
 var ad_source="snap   ";
 var action_url="https://script.google.com/macros/s/AKfycbzrPrqNbko42H-MFK98d618CoB3VIHNWcRjlkfcvHoXskpiOHb-1iIbhkh6PF3wYX2JUA/exec";
