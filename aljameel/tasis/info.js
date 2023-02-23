ea0b75707b46ecfc59605f3e50b1f223f27136e5
ForsanNemah <forsan20172017@gmail.com>
2023-02-23 13:19:43 +0300

    add

--- aljameel/tasis/info.js
+++ aljameel/tasis/info.js
@@ -5,7 +5,7 @@ var phn="966563615558";
 var end_date="29-1-2023";
 var end_time="12:00";
 var ad_source="من الاعلان ";
-var action_url="https://script.google.com/macros/s/AKfycbx-YqL54f54pRUm79bhxOYMBIw9z1Cj_Z_osO2O11umskNrfqPcHJiGDzWx0RXY_97G/exec";
+var action_url="https://script.google.com/macros/s/AKfycbyx1krvzzhbMvCZpK5SWqMjiGlX7dK7mEH5LNk3QRo0SRW-WT3yyTpAk8YSj8RUWkiFuw/exec";
 
 
 
