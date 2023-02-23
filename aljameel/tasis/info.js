0b489c98d9c6ffc58971cc57d9bcbdce33a18b90
ForsanNemah <forsan20172017@gmail.com>
2023-02-23 12:31:49 +0300

    new jameel

---
+++ aljameel/tasis/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966563615558";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="من الاعلان ";
+var action_url="https://script.google.com/macros/s/AKfycbx-YqL54f54pRUm79bhxOYMBIw9z1Cj_Z_osO2O11umskNrfqPcHJiGDzWx0RXY_97G/exec";
+
+
+
+
+
+
+
+ 
