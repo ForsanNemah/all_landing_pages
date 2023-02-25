bc831a6824764d019779ba191a723e86c7be607b
ForsanNemah <forsan20172017@gmail.com>
2023-02-25 15:55:34 +0300

    ephotial twitter 2

---
+++ ephoria/new/twitter/skin2/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966500663318";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbx_GX-PZV28-Cawhs8cj4Apyqsd8h-ARTUAoF431qm_lZkdC3yM4KOWZ1w8k5BtR8YW/exec";
+
+
+
+
+
+
+
+ 
---
+++ ephoria/new/twitter/skin3/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966500663318";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbx1yJ7CwJ_KZxl13IQSyFgZM0ov71KECKNeQEJvjvUNXSJdjVpoukr022Z763TPKzhZ/exec";
+
+
+
+
+
+
+
+ 
