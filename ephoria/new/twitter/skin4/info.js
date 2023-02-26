63e717b45c862331f3e11174afae1122471283ec
ForsanNemah <forsan20172017@gmail.com>
2023-02-26 15:54:08 +0300

    add skin 4

---
+++ ephoria/new/twitter/skin4/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966500663318";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbwqXaLUsG86wk19RDW6zFB_rH2KBjtlb5Yk9gZVa51HW1RORZcZi-pX-P6t0hTwu2QQ/exec";
+
+
+
+
+
+
+
+ 
