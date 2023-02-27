556ae79b05252893e64ddbc5e67494f14f1933e7
ForsanNemah <forsan20172017@gmail.com>
2023-02-27 12:06:11 +0300

    Update index.php

--- sultpro/index.php
+++ sultpro/index.php
@@ -393,7 +393,7 @@ include 'info.php';
     <footer class="page-footer font-small blue">
 
 <!-- Copyright -->
-<div class="footer-copyright text-center py-3">© 2022 جميع الحقوق محفوظة:
+<div class="footer-copyright text-center py-3">    جميع الحقوق محفوظة:
   <a href="https://wmc-ksa.com/">  شركة النافذة للتسويق الالكتروني </a>
 </div>
 <!-- Copyright -->
