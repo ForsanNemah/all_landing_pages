5d1cf63a66ce04e29e40d7e03eadbca90b970951
ForsanNemah <forsan20172017@gmail.com>
2023-03-04 14:32:53 +0300

    all

---
+++ taib/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966567777015";
+var end_date="1-5-2022";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbxiMv9Mljv4ptUJuwVwh8IGZ5GmHLM7LTM5MLGUVolmb1U_Rj8QRFnRXGsdIs6DzKGM/exec";
+
+
+
+
+
+
+ 
