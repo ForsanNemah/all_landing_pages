5c8fc4666aedf030d8762796c0b393ba6742f9dd
ForsanNemah <forsan20172017@gmail.com>
2023-03-08 16:37:51 +0300

    moh

---
+++ moh/moh1/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="96653461224";
+var end_date="1-5-2022";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbzJuIIO0NI9AXDNp_cVcRj-YjBCUnN_xK8xLu5WhcwKI6RyMUag5_rzkNDQCACeQKWYnA/exec";
+
+
+
+
+
+
+ 
