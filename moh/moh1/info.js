5023c756ade67ab7e2d4a3125c9689349b6bc3f6
ForsanNemah <forsan20172017@gmail.com>
2023-03-08 17:29:18 +0300

    moh2

--- moh/moh1/info.js
+++ moh/moh1/info.js
@@ -2,7 +2,7 @@
 
 
 var phn="96653461224";
-var end_date="1-5-2022";
+var end_date="20-3-2023";
 var end_time="12:00";
 var ad_source="snap   ";
 var action_url="https://script.google.com/macros/s/AKfycbzJuIIO0NI9AXDNp_cVcRj-YjBCUnN_xK8xLu5WhcwKI6RyMUag5_rzkNDQCACeQKWYnA/exec";
---
+++ moh/moh2/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966531444001";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbxA4RGnESrcHgiIkECf5vW8zqjSyHmyHECoEk-MIdBcvKuetamQFEPFZapYlpHmDNNdQQ/exec";
+
+
+
+
+
+
+ 
