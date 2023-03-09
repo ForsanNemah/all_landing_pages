fe2d82b1d68b8dd6262efee72eb18f1f4ba157a7
ForsanNemah <forsan20172017@gmail.com>
2023-03-09 13:13:20 +0300

    Update info.js

--- moh/moh5/moh5/info.js
+++ moh/moh5/moh5/info.js
@@ -4,7 +4,7 @@
 var phn="966509808095";
 var end_date="20-3-2023";
 var end_time="12:00";
-var ad_source="snap   ";
+var ad_source="snap  2  ";
 var action_url="https://script.google.com/macros/s/AKfycbx8_6iyhckUzIlUDtPavt8Q89W2917GAEZRlf29KFZh_mfj3tokkU9G62wY1mv_Qt4F/exec";
 
 
