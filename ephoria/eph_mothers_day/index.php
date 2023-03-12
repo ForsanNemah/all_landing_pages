0a91e2d7f411f3368f1247206a59e9195b4be95d
ForsanNemah <forsan20172017@gmail.com>
2023-03-12 23:06:34 +0300

    added

--- ephoria/eph_mothers_day/index.php
+++ ephoria/eph_mothers_day/index.php
@@ -99,23 +99,32 @@ include 'info.php';
 
 
 <script type="text/javascript" src="info.js"></script>
-<!-- Snap Pixel Code -->
-<script type='text/javascript'>
-(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
-{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
-a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
-r.src=n;var u=t.getElementsByTagName(s)[0];
-u.parentNode.insertBefore(r,u);})(window,document,
-'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'd5491495-876a-4cb6-b0e3-14aa8bd4b711', {
-'user_email': '_INSERT_USER_EMAIL_'
-});
 
-snaptr('track', 'PAGE_VIEW');
 
-</script>
-<!-- End Snap Pixel Code -->
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 </head>
 <body>
   
