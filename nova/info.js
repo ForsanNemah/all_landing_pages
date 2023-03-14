751ea2d6ca0a929a5c69138d5a683bac91b5497c
ForsanNemah <forsan20172017@gmail.com>
2023-03-14 16:03:52 +0300

    nova

---
+++ nova/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="9660597017860";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycby8fILTfiBrFIWnEqG2uWoDE4mHtdi1GPFysbXm-sa27Co_iacINpHUdONyw-ZPrCDj-Q/exec";
+
+
+
+
+
+
+ 
