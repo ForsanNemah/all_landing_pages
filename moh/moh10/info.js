1ffdd5a095ba3004d7422a8eb95bc4bc165fc7d4
ForsanNemah <forsan20172017@gmail.com>
2023-03-15 11:13:52 +0300

    add

---
+++ moh/moh10/info.js
@@ -0,0 +1,14 @@
+ 
+
+
+var phn="966509808095";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycby8Lc-gdMt1S68WGG7IU3V05_qjlQSorVQq3j24AQ3u5AxtKeERAzKGlVOK85Sal_wr/exec";
+
+
+
+
+
+ 
