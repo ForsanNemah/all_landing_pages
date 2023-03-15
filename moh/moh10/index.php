216838552231c5e19d107e0e794900046f0f77d8
ForsanNemah <forsan20172017@gmail.com>
2023-03-15 11:18:39 +0300

    add

--- moh/moh10/index.php
+++ moh/moh10/index.php
@@ -97,24 +97,28 @@ include 'info.php';
 
 
         <script type="text/javascript" src="info.js"></script>
+        
 
-        <script type='text/javascript'>
-(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
-{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
-a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
-r.src=n;var u=t.getElementsByTagName(s)[0];
-u.parentNode.insertBefore(r,u);})(window,document,
-'https://sc-static.net/scevent.min.js');
+ 
 
-snaptr('init', 'fcd57a7e-306f-4727-a866-e995d7a94c9f', {
-'user_email': '_INSERT_USER_EMAIL_'
-});
 
-snaptr('track', 'PAGE_VIEW');
 
-</script>
-<!-- End Snap Pixel Code -->
-<!-- End Snap Pixel Code -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 </head>
 <body>
   
