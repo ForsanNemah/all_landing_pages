302d16b4569419e572b33bfa7ebea4b5df9ae6d2
ForsanNemah <forsan20172017@gmail.com>
2023-03-17 23:07:02 +0300

    maple 2

---
+++ maple/maple_ramadan2/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966590990966";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="  1  ";
+var action_url="https://script.google.com/macros/s/AKfycbwIBz4qutPvIy1ySZMz3c2ubbmcA3ADjIfxOviQwqj2TYatnPTHcuZ3_1zeCopTWa4Q/exec";
+
+
+
+
+
+
+
+ 
