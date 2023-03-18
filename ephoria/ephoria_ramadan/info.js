ea609382b19c394a5a54ce888f668e62ca62e4f9
ForsanNemah <forsan20172017@gmail.com>
2023-03-19 01:57:25 +0300

    a

---
+++ ephoria/ephoria_ramadan/info.js
@@ -0,0 +1,17 @@
+ 
+
+
+
+
+var phn="966594944603";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbxtI2QyF_kRrvb20vpYHm4u4axQVzjoAf1ICm9sOP4vvobIFRhJa8GgCpyZjF5ZpMhQ/exec";
+
+
+
+
+
+
+ 
