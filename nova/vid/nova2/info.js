13a56a203ad028146eecb9fcc7dddb5fa1f7c2c2
ForsanNemah <forsan20172017@gmail.com>
2023-03-19 18:03:46 +0300

    add

---
+++ nova/vid/nova2/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="9660597017860";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap vid 2  ";
+var action_url="https://script.google.com/macros/s/AKfycbyag7Qq-ZdJiDCgcXsnOrms3_lp5Yn53Nab0b_Hq1vxJUOCh6lPch1DhIiSpeL3YPHz/exec";
+
+
+
+
+
+
+ 
