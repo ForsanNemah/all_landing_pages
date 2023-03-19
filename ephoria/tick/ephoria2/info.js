1eee511e5096cbcc641414b318be131a8a846de0
ForsanNemah <forsan20172017@gmail.com>
2023-03-19 14:56:38 +0300

    ephoria teck tock

---
+++ ephoria/tick/ephoria2/info.js
@@ -0,0 +1,18 @@
+ 
+
+
+
+
+var phn="966594944603";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbwA6qOuWOlpEw6ahakaVR0234yZTUg492338jeixX0zPB625wZ7JylFkr0TBO6fOFFWUA/exec";
+
+
+
+
+
+
+
+ 
---
+++ ephoria/tick/ephoria_ramadan/info.js
@@ -0,0 +1,17 @@
+ 
+
+
+
+
+var phn="966594944603";
+var msg="تم التسجيل بنجاح ";
+var end_date="27-1-2023";
+var end_time="12:00";
+var action_url="https://script.google.com/macros/s/AKfycbx5zd4g_OUowceyVFZvwSzAvUyzWIyvQBnCtwz_5vvT6knl2r0W9z7AEHnBTVoI5Ng8/exec";
+
+
+
+
+
+
+ 
