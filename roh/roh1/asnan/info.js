96f84247e508f4b1964163dd2975f3b99ed119b7
ForsanNemah <forsan20172017@gmail.com>
2023-03-23 23:11:48 +0300

    add rohimi asnan

---
+++ roh/roh1/asnan/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966531290801";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbyrum03M60qQZhxpOet-x86u8HmIyY1414kiDEjsWe3KVhwtaruz9Hjjh4XoYjOeL31dQ/exec";
+
+
+
+
+
+
+ 
