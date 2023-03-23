bc9386f7225846cafb78e45ce1b4a6a5bd2feb85
ForsanNemah <forsan20172017@gmail.com>
2023-03-23 23:16:30 +0300

    added

--- roh/roh1/asnan/info.js
+++ roh/roh1/asnan/info.js
@@ -1,11 +1,11 @@
  
 
 
-var phn="966531290801";
+var phn="966537203335";
 var end_date="20-3-2023";
 var end_time="12:00";
 var ad_source="snap   ";
-var action_url="https://script.google.com/macros/s/AKfycbyrum03M60qQZhxpOet-x86u8HmIyY1414kiDEjsWe3KVhwtaruz9Hjjh4XoYjOeL31dQ/exec";
+var action_url="https://script.google.com/macros/s/AKfycbzgdfkM82wdz2x8IJYmnvoIv0-X7f_UHeuRAuvvJHzon2c2FbjsNqydAEnGOlaCUyVx/exec";
 
 
 
