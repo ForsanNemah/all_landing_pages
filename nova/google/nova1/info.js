27ea0bf3cca084e49871e2c19eca6ae57b311804
ForsanNemah <forsan20172017@gmail.com>
2023-03-23 17:11:35 +0300

    nova

---
+++ nova/google/nova1/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966594741068";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbxoEA0FuKPKiUcGk_P5kW3OaUmjqA17L-TGucjSBQbTny9DCY9uXfZqxwXap4Wre6b5/exec";
+
+
+
+
+
+
+ 
---
+++ nova/google/nova2/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966592064095";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycby3FgU7C06IzfENdnzJJ9D5aqzu87tsjOvOqWsqpgdbK90OVoWHQtjwgxySG9W_J8MD/exec";
+
+
+
+
+
+
+ 
