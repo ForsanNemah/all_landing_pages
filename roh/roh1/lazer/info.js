355691eb9ff09706c0f76a4e169d5ab24e583c11
ForsanNemah <forsan20172017@gmail.com>
2023-03-24 02:47:19 +0300

    added

---
+++ roh/roh1/lazer/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966537203335";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbywslSOkwO5v2pM56BP5qitnCogy7jO3eJCGGDT40iyTo0qyOjkpjWfi5dqtBrCJFGb/exec";
+
+
+
+
+
+
+ 
