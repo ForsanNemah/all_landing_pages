50aa9868ef1d0970e752296c18722ec39907ad3f
ForsanNemah <forsan20172017@gmail.com>
2023-03-24 16:34:40 +0300

    added

---
+++ roh/roh1/skin/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966537203335";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbyMG7ka8v0br2wm-m6i14Z0Xqlf6yA0l8PMVTyCtRRvkkAmhz2hQ0m-bJVdzW9ikARIcw/exec";
+
+
+
+
+
+
+ 
