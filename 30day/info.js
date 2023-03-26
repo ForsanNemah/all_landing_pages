05f32512018d73a6112c2b2c7371505617566b15
ForsanNemah <forsan20172017@gmail.com>
2023-03-26 14:04:33 +0300

    30 day

---
+++ 30day/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966501699050";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbwiNWj6OZijpH4STtgifWiuTFvc68KkHzMwFUWP1YgGFTKd1_3ztcWQ18W6HnkHCq_j/exec";
+
+
+
+
+
+
+ 
