6783440ab569117002fa005e252246f29c56b055
ForsanNemah <forsan20172017@gmail.com>
2023-03-29 15:42:15 +0300

    add

---
+++ shar/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966559965998";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbw4tJ3Npb6Mk-3OoFdhnXJGg2vLCBpuMSFq5stBKkus6OgeBZwGm2gOqudiM9Z3oevk/exec";
+
+
+
+
+
+
+ 
