0554a699ded056045986b411ae0d8dfe978c0f5c
ForsanNemah <forsan20172017@gmail.com>
2023-03-30 23:42:34 +0300

    aljameel ramdan

---
+++ aljameel/aljameel_ramadan/info.js
@@ -0,0 +1,16 @@
+ 
+
+
+var phn="966563615558";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="من الاعلان ";
+var action_url="https://script.google.com/macros/s/AKfycbyDog6PgNTDGMjZKxJMg23GluobYN6qflJEk0apB6XhhlTAmBiTk7cNsD8U-kYBym-_/exec";
+
+
+
+
+
+
+
+ 
---
+++ car_care/car_care/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966555226279";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbwwGYzQKRKd-mccKXtt5tlEiTv_cU9RF0hWPu2n23w5Wul4OuZXxH2fH3oCVZLyOXhKHA/exec";
+
+
+
+
+
+
+ 
