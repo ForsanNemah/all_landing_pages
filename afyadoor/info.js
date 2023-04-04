56712968e3c2e1dfb8f0496fe4c6e5d693272115
ForsanNemah <forsan20172017@gmail.com>
2023-04-04 23:38:21 +0300

    added

---
+++ afyadoor/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966548817383";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap chat ";
+var action_url="https://script.google.com/macros/s/AKfycbxcXspEvVjegdi_bSG-fTWOiv3nSA7frgrgdhC6gNM234bdshPZw9C7nYGtu2GQVKXB0A/exec";
+
+
+
+
+
+
+ 
