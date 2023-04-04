addb5aacda96268ebe3b278332f71b44db73f54e
ForsanNemah <forsan20172017@gmail.com>
2023-04-04 07:29:23 +0300

    added

--- drsaeed/index.php
+++ drsaeed/index.php
@@ -675,7 +675,7 @@ var datetime = "" + currentdate.getDate() + "/"
     
 	$(':input[type=submit]').prop('disabled', true)
   $('#today').attr('value', datetime )
-  $('#w_link_id').attr('value', 'wa.me'+$("#phn_id").val() )
+  $('#w_link_id').attr('value', 'wa.me/'+$("#phn_id").val() )
   element.style.display = "block"
   //alert("progress")
     e.preventDefault()
