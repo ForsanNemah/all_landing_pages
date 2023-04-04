fff414f6fd888e96b02a74674157e47f7f7b81fa
ForsanNemah <forsan20172017@gmail.com>
2023-04-04 14:14:23 +0300

    4san

---
+++ 4sanasia/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966555879644";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap chat ";
+var action_url="https://script.google.com/macros/s/AKfycbyw0mzp-PJBqaBsA00YmME7_pXaQYLEK2jTnHpfaQQwVWG_kg6qMN_ScrUb0CQipqrO/exec";
+
+
+
+
+
+
+ 
