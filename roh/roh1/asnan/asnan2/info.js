063778cd4255ebadf53a4c8971fc3421a216bb3b
ForsanNemah <forsan20172017@gmail.com>
2023-04-05 16:19:14 +0300

    add

---
+++ roh/roh1/asnan/asnan2/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966537203335";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbzMHBg7cSBBlTMNABhl7Azc6RCYCIb1jFv3Fi8EjbCUVcd91Ci8Uoq0ikL4PsgOYh5hHg/exec";
+
+
+
+
+
+
+ 
