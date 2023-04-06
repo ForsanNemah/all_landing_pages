c4311d275e3ab7bdf98906d38e6699eca706118f
ForsanNemah <forsan20172017@gmail.com>
2023-04-06 15:14:19 +0300

    added

---
+++ car_care/car_care2/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966550878966";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap   ";
+var action_url="https://script.google.com/macros/s/AKfycbxwDYbenImTAsJ9eHWWh9FUBsg1pbqpZF3MCfIL9QuKeGZdHTfjKjyjozcEWhKGkV9bQg/exec";
+
+
+
+
+
+
+ 
