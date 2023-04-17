57f254fb53d5f0a4f2d6478d97f540ab2b7853ec
ForsanNemah <forsan20172017@gmail.com>
2023-04-17 05:15:33 +0300

    add

--- ephoria/ephoria_ramadan/index.php
+++ ephoria/ephoria_ramadan/index.php
@@ -50,7 +50,28 @@
         <!-- Favicon and touch icons -->
        
 		<script type="text/javascript" src="info.js"></script>
- 
+
+
+
+
+
+ <!-- Snap Pixel Code -->
+<script type='text/javascript'>
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+
+snaptr('init', '366a9171-f6e5-4f6c-917e-0e2f92b9da4d', {
+'user_email': '_INSERT_USER_EMAIL_'
+});
+
+snaptr('track', 'PAGE_VIEW');
+
+</script>
+<!-- End Snap Pixel Code -->
 
     </head>
 
