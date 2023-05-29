05ae2f71ad0b6656334862eb33e56a5d17456204
ForsanNemah <forsan20172017@gmail.com>
2023-05-29 15:46:31 +0300

    add

--- taib/taib2/info.js
+++ taib/taib2/info.js
@@ -4,7 +4,7 @@
 var phn="966560260015";
 var end_date="1-5-2022";
 var end_time="12:00";
-var ad_source="snap   ";
+var ad_source="snap  2  ";
 var action_url="https://script.google.com/macros/s/AKfycbxiMv9Mljv4ptUJuwVwh8IGZ5GmHLM7LTM5MLGUVolmb1U_Rj8QRFnRXGsdIs6DzKGM/exec";
 
 
