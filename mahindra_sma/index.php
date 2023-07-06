611e4a4fd8a6a4db46911d04e4a2a52cca4e7601
ForsanNemah <forsan20172017@gmail.com>
2023-07-06 13:37:42 +0300

    add

---
+++ mahindra_sma/index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="headline">
+          <h1>Mahindra Saudi Social Media Links</h1>
+        </div>
+        <div class="social-buttons">        
+        <!-- facebook  Button -->
+                <a href="https://www.facebook.com/Mahindra_saudi" target="blank" class="social-margin"> 
+                  <div class="social-icon facebook">
+                    <i class="fa fa-facebook" aria-hidden="true"></i> 
+                  </div>
+                </a>
+                <!-- pinterest Button -->
+                
+                <!-- LinkedIn Button -->
+                <a href="https://www.linkedin.com/company/mahindra-saudi/" class="social-margin" target="blank">
+                  <div class="social-icon linkedin">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                  </div> 
+                </a>
+              <!-- Github Button -->
+              
+                <!-- Youtube Button -->
+                <a href="https://youtube.com/@mahindra_saudi" target="blank"  class="social-margin">
+                  <div class="social-icon youtube">
+                    <i class="fa fa-youtube" aria-hidden="true"></i>
+                  </div> 
+                </a>
+                <!-- Behance Button -->
+                
+                <!-- Soundcloud Button -->
+            
+          <!-- TwitterButton -->
+                <a href="https://twitter.com/mahindra_saudi" target="blank" class="social-margin">
+                  <div class="social-icon twitter">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                  </div> 
+                </a>
+
+
+
+
+
+
+
+                <a href="https://twitter.com/mahindra_saudi" target="blank" class="social-margin">
+                  <div class="social-icon snapchat">
+                    <i class="fa fa-snapchat" aria-hidden="true"></i>
+                  </div> 
+                </a>
+
+
+
+
+        </div>
+        </div>
+</body>
+</html>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+html,body{
+  margin:0;
+  height:100%;
+  font-family: 'Montserrat', sans-serif;
+}
+.container{
+  padding-top:30px;
+  min-height:100%;
+  text-align:center;
+  background: black;
+}
+
+.headline{
+  text-align:center;
+ 
+}
+.headline  h1{
+    font-size:35px;
+    font-family: 'Montserrat', sans-serif;
+    color:#fff;
+  }
+
+.social-buttons{
+  display:inline-block;
+  background: rgba(256,256,256,0.5);
+  padding:20px;
+  padding-bottom: 5px;
+  border-radius:10px;
+  text-align:center;
+  margin:20px 10px;
+  box-shadow: 0px 0px 32px -7px #00199f;
+}
+  
+/* Helper class to divide the icons */
+.social-margin {
+  margin-right: 15px;
+}
+
+a,
+a:hover,
+a:focus,
+a:active {
+  text-decoration: none;
+}
+
+.social-icon {
+  margin-bottom: 15px;
+   box-sizing: border-box;
+  -moz-border-radius: 138px;
+  -webkit-border-radius: 138px;
+  border-radius: 138px;
+  border: 5px solid;
+  text-align: center;
+  width: 50px;
+  height: 50px;
+  display: inline-block;
+  line-height: 1px;
+  padding-top: 11px;
+  transition: all 0.5s;
+}
+.social-icon:hover {
+    transform: rotate(360deg)scale(1.3);
+  }
+  /* Facebook Button Styling */
+ .facebook {
+    font-size: 22px;
+    padding-top: 9px;
+    border-color: #3b5998;
+    background-color: #3b5998;
+    color: #ffffff;
+   
+  }
+  .facebook:hover {
+    background-color: #ffffff;
+    color: #3b5998;
+  }
+  /* Twitter Button Styling */
+.twitter {
+    font-size: 22px;
+    padding-top: 10px;
+    padding-left: 2px;
+    border-color: #55acee;
+    background-color: #55acee;
+    color: #ffffff;
+    
+  }
+  .twitter:hover {
+    background-color: #ffffff;
+    color: #55acee;
+  }
+  /* Google+ Button Styling */
+  .google-plus {
+    font-size: 22px;
+    padding-top: 9px;
+    padding-left: 2px;
+    background-color: #dd4b39;
+    color: #ffffff;
+    border-color: #dd4b39;
+    
+  }
+  .google-plus :hover {
+    background-color: #ffffff;
+    color: #dd4b39;
+  }
+  /* Linkedin Button Styling */
+  .linkedin {
+    font-size: 24px;
+    padding-top: 8px;
+    padding-left: 1px;
+    background-color: #0976b4;
+    color: #ffffff;
+    border-color: #0976b4;
+    
+  }
+  .linkedin :hover {
+    background-color: #ffffff;
+    color: #0976b4;
+  }
+  /* Pinterest Button Styling */
+.pinterest {
+    font-size: 22px;
+    padding-top: 9px;
+    background-color:  #cb2027;
+    color: #ffffff;
+    border-color:  #cb2027;
+    
+  }
+  .pinterest:hover {
+    background-color: #ffffff;
+    color:  #cb2027;
+  }
+  /* Behance Button Styling */
+  .behance {
+    font-size: 22px;
+    padding-top: 9px;
+    background-color: #1769ff;
+    color: #ffffff;
+    border-color: #1769ff;
+    
+  }
+  .behance :hover {
+    background-color: #ffffff;
+    color: #1769ff;
+  }
+
+  /* Github Button Styling */
+ .github {
+    font-size: 22px;
+    padding-top: 9px;
+    background-color: #4183c4;
+    color: #ffffff;
+    border-color: #4183c4;
+    
+  }
+  .github:hover {
+    background-color: #ffffff;
+    color: #4183c4;
+  }
+
+  /* Youtube Button Styling */
+ .youtube {
+    font-size: 22px;
+    padding-top: 9px;
+    padding-left: 0px;
+    background-color: #bb0000;
+    color: #ffffff;
+    border-color: #bb0000;
+    
+  }
+  .youtube  :hover {
+    background-color: #ffffff;
+    color: #bb0000;
+  }
+
+
+
+
+
+
+  .snapchat {
+    font-size: 22px;
+    padding-top: 9px;
+    padding-left: 0px;
+    background-color: #FFFC00;
+    color: #ffffff;
+    border-color: #bb0000;
+    
+  }
+  .snapchat  :hover {
+    background-color: #ffffff;
+    color: #bb0000;
+  }
+
+
+
+
+
+
+
+  /* Soundcloud Button Styling */
+  .soundcloud {
+    font-size: 22px;
+    padding-top: 9px;
+    padding-left: 0px;
+    background-color: #ff3a00;
+    color: #ffffff;
+    border-color: #ff3a00;
+    
+  }
+  .soundcloud :hover {
+    background-color: #ffffff;
+    color: #ff3a00;
+  }
+
+
+
+</style>
---
+++ visa/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966569414666";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap chat ";
+var action_url="https://script.google.com/macros/s/AKfycbyKK065Cql6l2-xzPycCrwVxTuKn9wZZso1jqLFNU4FfmbapkYE9dFFrQTNaJQok_HJ/exec";
+
+
+
+
+
+
+ 
