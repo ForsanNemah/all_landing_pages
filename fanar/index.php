c29f7c110a835d2f861ded0a37f8f292b3a5583c
ForsanNemah <forsan20172017@gmail.com>
2023-07-19 11:22:33 +0300

    added

--- fanar/index.php
+++ fanar/index.php
@@ -609,6 +609,13 @@ include 'info.php';
 <!-- Copyright -->
 <div class="footer-copyright text-center py-3">    جميع الحقوق محفوظة:
   <a href="https://wmc-ksa.com/">  شركة النافذة للتسويق الالكتروني </a>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <a href="privcy.php">    سياسة الخصوصية  </a>
 </div>
 <!-- Copyright -->
 
