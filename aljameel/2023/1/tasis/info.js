53048b17f6f9bbcef4d9378ec9c86a4abfeb3898
ForsanNemah <forsan20172017@gmail.com>
2023-07-24 16:49:19 +0300

    add

---
+++ aljameel/2023/1/tasis/info.js
@@ -0,0 +1,17 @@
+ 
+
+
+var phn="966563615558";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="من الاعلان ";
+var action_url="https://script.google.com/macros/s/AKfycbyx1krvzzhbMvCZpK5SWqMjiGlX7dK7mEH5LNk3QRo0SRW-WT3yyTpAk8YSj8RUWkiFuw/exec";
+
+
+
+
+
+
+
+
+ 
---
+++ aljameel/2023/2/info.js
@@ -0,0 +1,17 @@
+ 
+
+
+var phn="966563615558";
+var end_date="29-1-2023";
+var end_time="12:00";
+var ad_source="2023/2  ";
+var action_url="https://script.google.com/macros/s/AKfycbz5nRh_7yqLiDFDiwUUoVizimf67gCxBiAR4Kq010SOs8aO9hMwfO4Bg3wUpeJQFcV9mQ/exec";
+
+
+
+
+
+
+
+
+ 
