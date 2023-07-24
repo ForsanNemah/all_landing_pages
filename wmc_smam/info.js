8d6534b154c6cc5c4b223368462c72ffc2ee4485
ForsanNemah <forsan20172017@gmail.com>
2023-07-24 13:32:04 +0300

    added

---
+++ wmc_smam/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966553907575";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap chat ";
+var action_url="https://script.google.com/macros/s/AKfycbxieOct-69a7F3sMcdG99CgNyK-DKz_ERn1-jQVy9X_7_61c6e_B3SXTXqcB2v-yBe9hQ/exec";
+
+
+
+
+
+
+ 
---
+++ wmc_smam/info.php
@@ -0,0 +1,172 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+ 
+$ad_source="snap";
+
+$logo_h='100px';
+$logo_w='20px';
+//echo $ad_source;
+
+
+
+
+
+
+
+try {
+
+
+
+    $items = array();
+
+
+
+$handle = fopen("services/asteqdam_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+    
+
+    "website_title"=>"    شركة النافذة-لجميع الخدمات التسويقية   ",
+     "sub1"=>"      الحمزة للسيارات  ",
+    "sub2"=>"أفضل عروض شانجان - شانجان جديدة بأفضل سعر   
+      ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>"  عرض 2500 ريال  ",
+ "sidebar2_msg"=>"  تجهيزات باحدث التقنيات     ",
+ "sidebar3_msg"=>"     2  كادر طبي متميز       ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+    "ev_logo_name"=>"    
+    
+    شركة النافذة لجميع الخدمات التسويقية 
+    ",
+
+
+    
+    "ev_form_sub1"=>"    
+    املأ النموذج الآن
+    
+    ",
+
+
+    "ev_form_sub2"=>"    
+    
+  وسيتم التواصل معك في اقل من 24 ساعة  
+    ",
+
+
+    
+    "lt_sub1"=>"    
+    
+
+
+    تحسين ظهور حسابات نشاطك التجاري  وتطوير محتواك  يسهل وصول علامتك التجارية لشريحتك المستهدفة مما يزيد مبيعاتك. 
+    ",
+
+
+    "lt_sub2"=>"    
+
+
+    تواصل معنا لنساعدك في هذا
+ 
+
+
+ 
+    ",
+
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+    
+  } catch(Exception $e) {
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+?>
