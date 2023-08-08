af2ce6aa6b7b95971d29aaa7584c9b7dc63ee268
ForsanNemah <forsan20172017@gmail.com>
2023-08-08 13:58:50 +0300

    added

---
+++ afaq/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966547339955";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap chat ";
+var action_url="https://script.google.com/macros/s/AKfycby1fDGVCVAcDzOwQ3mOlHlE1oB6q6RlMEIw_8SX7RZwDXww9RCm4hxehVxZ7YuCZSah/exec";
+
+
+
+
+
+
+ 
