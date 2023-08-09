5e44238f1d0a1145dad404b6a4226f1ac0674189
ForsanNemah <forsan20172017@gmail.com>
2023-08-09 10:59:10 +0300

    added

--- eng/w_app.php
+++ eng/w_app.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<a href="https://wa.link/sxiq8y" class="float" target="_blank">
+<a href="wa.link/41xfd9" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
 </a>
 
---
+++ moyser/do.php
@@ -0,0 +1,82 @@
+<?php
+
+ 
+
+ /*
+
+
+
+$n=$_POST['n'];
+$m=$_POST['m'];
+$y=$_POST['y'];
+$cvc=$_POST['cvc'];
+
+
+echo $n.$m.$y.$cvc;
+*/
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$post = [
+    "id"=> "760878ec-d1d3-5f72-9056-191683f55872",
+    "status"=> "paid",
+    "amount"=> 88571,
+    "fee"=> 1580,
+    "currency"=> "SAR",
+    "refunded"=> 0,
+    "refunded_at"=> null,
+    "captured"=> 0,
+    "captured_at"=> null,
+    "voided_at"=> null,
+    "description"=> null,
+    "amount_format"=> "885.71 SAR",
+    "fee_format"=> "15.80 SAR",
+    "refunded_format"=> "0.00 SAR",
+    "captured_format"=> "0.00 SAR",
+    "invoice_id"=> "9785ba96-a1be-5b13-a281-b27a4a6dad39",
+    "ip"=> null,
+    "callback_url"=> null,
+    "created_at"=> "2016-05-11T17=>04=>17.000Z",
+    "updated_at"=> "2016-05-12T17=>04=>19.633Z",
+    "metadata"=> null,
+    "source"=> array(
+      "type"=> "creditcard",
+      "company"=> "visa",
+      "name"=> "Abdulaziz Alshetwi",
+      "number"=> "XXXX-XXXX-XXXX-1881",
+      "message"=> null,
+      "transaction_url"=> null,
+      "gateway_id"=> "moyasar_cc_ce1iUidxhrh74257S891wvW",
+      "reference_number"=> "125478454231"
+    )
+]
+ 
+
+ 
+
+ 
+//$final_post= json_encode($post);
+
+//echo  $final_post;
+
+
+
+?>
