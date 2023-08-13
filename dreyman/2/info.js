80dc0d93393f3e38ecd4d15302a5dbe6e8b2bf64
ForsanNemah <forsan20172017@gmail.com>
2023-08-13 18:45:36 +0300

    added

---
+++ dreyman/2/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966568430828";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap 2";
+var action_url="https://script.google.com/macros/s/AKfycbwX5sqGmJdqWQSnpnZ-3UPg-5I9l2rHpinvMRwgR9KMRJC4YEyjMF0Ock4QGUv3NNccyw/exec";
+
+
+
+
+
+
+ 
