3cab03a7220c4e31178fd08a5c2fb1aa7b29a2a9
ForsanNemah <forsan20172017@gmail.com>
2023-08-14 11:10:37 +0300

    added

---
+++ dralamey/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966568430828";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap";
+var action_url="https://script.google.com/macros/s/AKfycbxhAH7QU6F8OyY7olGRWlW-qHzYYb7ZhUv010x0aYUPGw0dHeVsXfBstEbQdzSGO2Q6/exec";
+
+
+
+
+
+
+ 
