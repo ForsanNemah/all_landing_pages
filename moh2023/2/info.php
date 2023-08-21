abdcaaf29b3a31972708e712356b9835e5c84fea
ForsanNemah <forsan20172017@gmail.com>
2023-08-21 17:20:36 +0300

    added

---
+++ moh2023/2/info.php
@@ -0,0 +1,172 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+ 
+$ad_source="snap 2";
+
+$logo_h='100px';
+$logo_w='20px';
+$sliders=0;
+//echo $ad_sources;
+
+
+
+
+
+
+
+try {
+
+
+
+    $items = array();
+
+
+
+$handle = fopen("services/asnan_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+    
+
+    "website_title"=>"        صفوة المهيدب لطب الاسنان     ",
+     "sub1"=>"      الحمزة للسيارات  ",
+    "sub2"=>"أفضل عروض شانجان - شانجان جديدة بأفضل سعر   
+      ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>"         خدماتنا       ",
+ "sidebar2_msg"=>"  تجهيزات باحدث التقنيات     ",
+ "sidebar3_msg"=>"     2  كادر طبي متميز       ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+    "ev_logo_name"=>"    
+    خصم 60% على جميع الخدمات 
+    ",
+
+
+    
+    "ev_form_sub1"=>"    
+    املأ النموذج الآن
+    
+    ",
+
+
+    "ev_form_sub2"=>"    
+    
+واحصل على العروض
+    ",
+
+
+    
+    "lt_sub1"=>"    
+    
+
+ 
+    ",
+
+
+    "lt_sub2"=>"    
+
+ 
+
+ 
+
+
+
+ 
+    ",
+
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+    
+  } catch(Exception $e) {
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+?>
---
+++ moh2023/4/skin/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966531197063";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap";
+var action_url="https://script.google.com/macros/s/AKfycbz1tJrU4MzVthDB2QOQz51Zya_RlfxCepUeuu2KwHBUOqlp-F9QalNfvSpb3Y9iH3Ee/exec";
+
+
+
+
+
+
+ 
