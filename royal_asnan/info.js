045121695f3c340122ed36886f44cd1fbae57e0e
ForsanNemah <forsan20172017@gmail.com>
2023-08-25 09:46:28 +0300

    Update info.js

--- royal_asnan/info.js
+++ royal_asnan/info.js
@@ -5,7 +5,7 @@ var phn="966531197063";
 var end_date="20-3-2023";
 var end_time="12:00";
 var ad_source="snap";
-var action_url="https://script.google.com/macros/s/AKfycbznPsNnKeOE4szK9ZZL_Sy3c7JQwVWmeVflukZytpz_Jir9kcbl85ZzzH20txbtGrDv3w/exec";
+var action_url="https://script.google.com/macros/s/AKfycbwu66nZXweQe70RJZhbrlwPNAyuLn4n4MgspuBigG5CmSgQr8GNM8JGHhS9nrHTT2-I/exec";
 
 
 
