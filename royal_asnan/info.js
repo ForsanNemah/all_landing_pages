9636023024b1039ba280af487fd284357828efca
ForsanNemah <forsan20172017@gmail.com>
2023-08-25 15:04:05 +0300

    ad

--- royal_asnan/info.js
+++ royal_asnan/info.js
@@ -5,7 +5,7 @@ var phn="966531197063";
 var end_date="20-3-2023";
 var end_time="12:00";
 var ad_source="snap";
-var action_url="https://script.google.com/macros/s/AKfycbwu66nZXweQe70RJZhbrlwPNAyuLn4n4MgspuBigG5CmSgQr8GNM8JGHhS9nrHTT2-I/exec";
+var action_url="https://script.google.com/macros/s/AKfycbw77a4F0YCy1JN4mCCsW4F-IJnDwC97ErTfy_5VmquGJD9tb9_-GiDQLHwSZCyojGq8/exec";
 
 
 
