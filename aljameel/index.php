cc6607814b0dfdb58465dafc173b096133e82b5d
ForsanNemah <forsan20172017@gmail.com>
2023-08-26 16:33:04 +0300

    added

--- aljameel/index.php
+++ aljameel/index.php
@@ -51,7 +51,37 @@
        
 		<script type="text/javascript" src="info.js"></script>
  
-    <script type="text/javascript" src="pixel/snap.js"></script>
+    <script>
+
+
+ 
+var event_phn=phn;
+
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+
+snaptr('init', 'c530b9a9-f8d4-48df-ba97-3958aae4f9a6', {
+    'user_phone_number': event_phn
+});
+
+snaptr('track', 'PAGE_VIEW');
+ 
+ 
+
+    </script>
+
+
+
+
+
+
+
+
+
     </head>
 
     <body  dir="rtl">
@@ -534,7 +564,7 @@ var datetime = "" + currentdate.getDate() + "/"
    event_phn=$("#phn_id").val();
    snaptr('track','SIGN_UP');
 
-   
+
     window.location.replace("https://wa.me/"+phn) ;
     
 }
