77f514ec83f496ec2daa8026a0366cca0db401b3
ForsanNemah <forsan20172017@gmail.com>
2023-08-26 17:45:45 +0300

    added

---
+++ meswak_hafer/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966531197063";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap";
+var action_url="https://script.google.com/macros/s/AKfycbz4ZOntS31YlzP3cq99NQDQZRasCN7qAoLDiZxcPAQWVHEt6tgF1FOiwcdjes1WgsX_/exec";
+
+
+
+
+
+
+ 
