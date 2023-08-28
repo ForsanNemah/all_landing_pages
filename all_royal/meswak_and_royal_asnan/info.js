2ba017a8d04f754e2e7f876fbf1eda9213e7cbd5
ForsanNemah <forsan20172017@gmail.com>
2023-08-29 00:08:56 +0300

    added

--- all_royal/meswak_and_royal_asnan/info.js
+++ all_royal/meswak_and_royal_asnan/info.js
@@ -5,7 +5,7 @@ var phn="966531197063";
 var end_date="20-3-2023";
 var end_time="12:00";
 var ad_source="snap";
-var action_url="https://script.google.com/macros/s/AKfycbxNxyiHEX7Pp65yDhV9CS7NVfKj3oINL4kKKbu3A2LjDJ2T77d4K9mAAwJkT-PKEvZp/exec";
+var action_url="https://script.google.com/macros/s/AKfycbzheJxhe04rfAOyCrGE2XJi_tw4TC2atU4nOlBwcGaf08gpaazROeUbQZ2KNu_qfsKN/exec";
 
 
 
