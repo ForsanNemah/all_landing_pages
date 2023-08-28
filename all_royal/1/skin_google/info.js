760ad2a84567c61cf22f4fc6ea6cb3667d75e016
ForsanNemah <forsan20172017@gmail.com>
2023-08-28 13:47:22 +0300

    added

---
+++ all_royal/1/skin_google/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966531197063";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap";
+var action_url="https://script.google.com/macros/s/AKfycbw8_RVGgaFgC1P1tb-JpgWJ9wNF44w4WRzc7vtuXmkGY9CzDnI49Y1LI3Ge168q-L8d/exec";
+
+
+
+
+
+
+ 
