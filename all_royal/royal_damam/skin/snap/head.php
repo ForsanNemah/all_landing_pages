68bc2080ae1097472ccc00cf39a548d6f27d0e48
ForsanNemah <forsan20172017@gmail.com>
2023-09-01 16:46:42 +0300

    a

--- all_royal/royal_damam/skin/snap/head.php
+++ all_royal/royal_damam/skin/snap/head.php
@@ -1,10 +1,15 @@
 
 
-<script type="text/javascript" src="info.js"></script>
+
 
 
 
 <head>
+
+
+
+<script type="text/javascript" src="info.js"></script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="title" content="">
