5dd1757858d2cc7437be7882a1829e28a8ba452c
ForsanNemah <forsan20172017@gmail.com>
2023-09-01 03:00:37 +0300

    add

---
+++ all_royal/royal_alrawdah/skin/info.php
@@ -0,0 +1,178 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+ 
+//$ad_source=echo "<script> alert(""); </script>";
+
+ 
+
+$logo_h='100px';
+$logo_w='20px';
+$sliders=0;
+$add_source= $_COOKIE['add_source'];
+$gmap_url= $_COOKIE['gmap_url'];
+
+//echo "<script> alert('".$gmap_url."'); </script>";
+
+ 
+
+
+
+
+
+try {
+
+
+
+    $items = array();
+
+
+
+$handle = fopen("services/asnan_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+$site_name=$_COOKIE['site_name'];
+
+
+$info = array(
+
+    
+
+    "website_title"=>$site_name,
+     "sub1"=>"      الحمزة للسيارات  ",
+    "sub2"=>"أفضل عروض شانجان - شانجان جديدة بأفضل سعر   
+      ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>$_COOKIE['sidebar1_msg'],
+ "sidebar2_msg"=>$_COOKIE['sidebar2_msg'],
+ "sidebar3_msg"=>$_COOKIE['sidebar3_msg'],
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+    "ev_logo_name"=>
+    $site_name
+    ,
+
+
+
+    
+    "ev_form_sub1"=>"    
+    املأ النموذج الآن
+    
+    ",
+
+
+    "ev_form_sub2"=>"    
+    
+واحصل على العروض
+    ",
+
+
+    
+    "lt_sub1"=>"    
+    
+
+ 
+    ",
+
+
+    "lt_sub2"=>"    
+
+ 
+
+ 
+
+
+
+ 
+    ",
+
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+    
+  } catch(Exception $e) {
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+?>
