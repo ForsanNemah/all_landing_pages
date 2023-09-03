d5a3c36df34a2a51182ae3a1f1bd48ee8754a793
ForsanNemah <forsan20172017@gmail.com>
2023-09-03 13:09:40 +0300

    aa

---
+++ dralamey/snap/info.js
@@ -0,0 +1,15 @@
+ 
+
+
+var phn="966568430828";
+var end_date="20-3-2023";
+var end_time="12:00";
+var ad_source="snap";
+var action_url="https://script.google.com/macros/s/AKfycbwKKyXsgrv7mKD2e3KVsppdZ-ERv27yZZuNRgoThXwa4CscUTk0-uR0nkThNph7EZk/exec";
+
+
+
+
+
+
+ 
---
+++ dralamey/snap/info.php
@@ -0,0 +1,171 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+ 
+$ad_source="snap";
+
+$logo_h='100px';
+$logo_w='20px';
+//echo $ad_source;
+
+
+
+
+
+
+
+try {
+
+
+
+    $items = array();
+
+
+
+$handle = fopen("services/mangrove_services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+$info = array(
+
+    
+
+    "website_title"=>"      عيادة الدكتور محمد العلامي       ",
+     "sub1"=>"      الحمزة للسيارات  ",
+    "sub2"=>"أفضل عروض شانجان - شانجان جديدة بأفضل سعر   
+      ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>"         خدماتنا       ",
+ "sidebar2_msg"=>"  تجهيزات باحدث التقنيات     ",
+ "sidebar3_msg"=>"     2  كادر طبي متميز       ",
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+    "ev_logo_name"=>"    
+    عيادة الدكتور محمد العلامي    
+    ",
+
+
+    
+    "ev_form_sub1"=>"    
+    املأ النموذج الآن
+    
+    ",
+
+
+    "ev_form_sub2"=>"    
+    
+واحصل على العروض
+    ",
+
+
+    
+    "lt_sub1"=>"    
+    
+
+  انت اجمل من ما تتصورين
+    ",
+
+
+    "lt_sub2"=>"    
+
+ 
+
+ 
+
+
+
+ 
+    ",
+
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+    
+  } catch(Exception $e) {
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+?>
