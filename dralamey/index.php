c783f4491ce8f85303ca9c9b83280346475713de
ForsanNemah <forsan20172017@gmail.com>
2023-09-05 16:05:19 +0300

    added

--- dralamey/index.php
+++ dralamey/index.php
@@ -454,6 +454,28 @@ include 'info.php';
 
 
                                    
+<div class="form-group " dir="rtl">
+                                        <select name="service" id="servicerb" class="form-control" aria-label="عرض زراعة الأسنان" required>
+
+                                        <?php
+
+include 'info.php';
+
+
+
+
+foreach ($items as $key => $value) {
+
+echo "<option value='$value'>$value</option>";
+
+}
+?>
+
+
+
+
+                                        </select>
+                                    </div>
 
 
 
