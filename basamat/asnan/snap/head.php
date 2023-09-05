57b15dbc3aaa73706bfa17af3d46d3729328ed44
ForsanNemah <forsan20172017@gmail.com>
2023-09-05 13:46:40 +0300

    add

--- basamat/asnan/snap/head.php
+++ basamat/asnan/snap/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'a8c10f73-7a8e-47c8-9bae-6cd4cf01e7bb', {
+snaptr('init', '53229950-c81c-4333-9ed3-1786ffa3d4a0', {
   'user_email': event_email,
     'user_phone_number': event_phn
 });
