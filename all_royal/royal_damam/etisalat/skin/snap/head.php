f6d29bbf60a2d691ac21849a61532db81e24d63d
ForsanNemah <forsan20172017@gmail.com>
2023-09-08 18:50:11 +0300

    added

--- all_royal/royal_damam/etisalat/skin/snap/head.php
+++ all_royal/royal_damam/etisalat/skin/snap/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', '664374b3-00f5-44df-94b9-c21b47c8728a', {
+snaptr('init', '31104507-bf96-4c29-a273-a1bad59360d8', {
   'user_email': event_email,
     'user_phone_number': event_phn
 });
