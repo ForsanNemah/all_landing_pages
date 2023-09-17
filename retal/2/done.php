6035438adec7ab863d10a1ffbc19ec8c8cab9b59
ForsanNemah <forsan20172017@gmail.com>
2023-09-17 23:34:26 +0300

    added

---
+++ retal/2/done.php
@@ -0,0 +1,34 @@
+
+<?php
+
+include 'head.php';
+
+ 
+?>
+
+<?php
+
+include 'logo_section.php';
+
+ 
+?>
+
+
+
+<p class="text-right " style="background: #D3D3D3;" dir="rtl">
+    
+     تم التسجيل بنجاح و سيتم التواصل معكم في اقرب وقت
+
+
+
+</p>
+
+
+
+
+<?php
+
+include 'footer.php';
+
+ 
+?>
