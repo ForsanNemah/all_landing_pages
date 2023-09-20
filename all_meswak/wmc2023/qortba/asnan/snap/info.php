2e798bb372b02a99f238e854386c4be1a1c71563
ForsanNemah <forsan20172017@gmail.com>
2023-09-20 12:52:02 +0300

    added

--- all_meswak/wmc2023/qortba/asnan/snap/info.php
+++ all_meswak/wmc2023/qortba/asnan/snap/info.php
@@ -18,7 +18,7 @@ $insta_url="https://www.instagram.com/dr.eman_elsharkawy/?hl=ar";
 $snap_url="https://www.instagram.com/dr.eman_elsharkawy/?hl=ar";
 $gmap_url="https://goo.gl/maps/S1Zko9MvKiYzqPQV9";
 
-$site_name="مجمع رويال الطبي-الرياض-فرع قرطبة-اسنان";
+$site_name="مجمع المسواك الطبي-الرياض-فرع قرطبة-اسنان";
 
 $sidebar1_msg="نحلم ونحقق";
 $sidebar2_msg="خدماتنا ";
