c8937276396337ecddaf29d48049c0338f30da18
ForsanNemah <forsan20172017@gmail.com>
2023-10-06 03:08:52 +0300

    Update index.php

--- all_meswak/wmc2023/gasan/index.php
+++ all_meswak/wmc2023/gasan/index.php
@@ -33,9 +33,9 @@ include 'head.php';
 
 
 <?php
-//include 'info.php';
+include 'info.php';
 //include 'cta/cta_call.php';
-include 'cta/cta_whatsapp.php';
+//include 'cta/cta_whatsapp.php';
 //include 'cta/cta_insta.php';
 //include 'cta/cta_snap.php';
 //include 'cta/cta_gmap.php';
