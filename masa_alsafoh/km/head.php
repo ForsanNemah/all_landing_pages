bab1749148db1b269fd78123db05a5912cbb47cf
ForsanNemah <forsan20172017@gmail.com>
2023-10-08 22:40:49 +0300

    add

--- masa_alsafoh/km/head.php
+++ masa_alsafoh/km/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'a8c10f73-7a8e-47c8-9bae-6cd4cf01e7bb');
+snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
 
 snaptr('track', 'PAGE_VIEW');
  
