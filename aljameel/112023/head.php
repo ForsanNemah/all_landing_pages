f69dacb15c73c232210fdc060e8589cb736883b1
ForsanNemah <forsan20172017@gmail.com>
2023-11-22 13:27:56 +0300

    aded

--- aljameel/112023/head.php
+++ aljameel/112023/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
+snaptr('init', 'c530b9a9-f8d4-48df-ba97-3958aae4f9a6');
 
 snaptr('track', 'PAGE_VIEW');
  
