8bfb6aaf33bc6991ddec7d9009f0c3e174bda7b9
ForsanNemah <forsan20172017@gmail.com>
2023-12-23 16:27:28 +0300

    j

--- twillow/index.php
+++ twillow/index.php
@@ -16,7 +16,7 @@ $token = "f6274ff85fb2d186aefbef35ec106b5b";
 $twilio = new Client($sid, $token);
 
 $message = $twilio->messages
-                  ->create("whatsapp:+966564844382", // to
+                  ->create("whatsapp:+966568430828", // to
                            [
                                "from" => "whatsapp:+966596945907",
                                "body" => "Hello there!"
