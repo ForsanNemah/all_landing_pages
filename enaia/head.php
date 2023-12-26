b9ba6e4b6b31c1f11f07bc12d0c0f93c1133b9ea
ForsanNemah <forsan20172017@gmail.com>
2023-12-26 12:40:18 +0300

    aa

--- enaia/head.php
+++ enaia/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
+snaptr('init', '195f3d5f-a24d-44aa-870b-eec5a550ff2d');
 
 snaptr('track', 'PAGE_VIEW');
  
