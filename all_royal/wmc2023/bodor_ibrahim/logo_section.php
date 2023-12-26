e28525326b35ea04737a40ceae0f7ebb83f2ddbb
ForsanNemah <forsan20172017@gmail.com>
2023-12-26 20:48:04 +0300

    a

--- all_royal/wmc2023/bodor_ibrahim/logo_section.php
+++ all_royal/wmc2023/bodor_ibrahim/logo_section.php
@@ -73,7 +73,7 @@ include 'info.php';
 
  
 
-//echo "  <h4 style='color:black'  >".$info['ev_logo_name']."</h4>";
+echo "  <h3 style='color:black'  >".$info['ev_logo_name']."</h3>";
 echo "  <h4 style='color:black' > ".$dr_name."</h4>";
 echo "  <h2 style='color:green' class='pulsate'  >".$add_title." </h2>";
 
