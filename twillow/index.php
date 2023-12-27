9e0ad71ee8dc32c2fdf040829c95416a6ff95308
ForsanNemah <forsan20172017@gmail.com>
2023-12-28 02:27:26 +0300

    ass

--- twillow/index.php
+++ twillow/index.php
@@ -12,7 +12,7 @@ use Twilio\Rest\Client;
  
 
 $sid    = "AC25e12df9a67265f35135b4e28b2e28d2";
-$token  = "[AuthToken]";
+$token  = "f6274ff85fb2d186aefbef35ec106b5b";
 $twilio = new Client($sid, $token);
 
 $message = $twilio->messages
