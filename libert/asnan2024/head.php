d650d01c5125800fea74cdf3d6a591c80886f657
ForsanNemah <forsan20172017@gmail.com>
2024-01-11 16:54:57 +0300

    aa

--- libert/asnan2024/head.php
+++ libert/asnan2024/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
+snaptr('init', '8679bf87-98b3-4716-a52e-05d05e4ace92');
 
 snaptr('track', 'PAGE_VIEW');
  
