381cfe1c93710029ae32913b5d22f7ba9c9977ea
ForsanNemah <forsan20172017@gmail.com>
2024-01-31 18:43:42 +0300

    add

---
+++ royal2024/Etisalat/derma/services_select.php
@@ -0,0 +1,36 @@
+ 
+
+
+
+
+
+
+
+
+                                    <div class="col-lg-12">
+
+<div class="st-form-field st-style1">
+  <label>الخدمة</label>
+ 
+
+  <select   name="service"  class="st_select1 "
+  
+      
+      <option></option>
+
+      
+<?php
+      foreach ($items as $key => $value) {
+
+echo "<option value='$value'>$value</option>";
+
+}
+?>
+
+
+
+    </select>
+</div>
+
+
+</div>
